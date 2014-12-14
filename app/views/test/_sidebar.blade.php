a3175eeb2807a5ba8ee892a7f46f4f0d7d04d355
Selim Reza <me@selimreza.com>
2014-12-14 17:19:37 +0600

    Admin Template for Code Base

---
+++ app/views/test/_sidebar.blade.php
@@ -0,0 +1,13 @@
+    <ul class="nav nav-list bs-docs-sidenav nav-collapse collapse">
+        <li class="active">
+            <a href="index.html"><i class="icon-chevron-right"></i> Dashboard</a>
+        </li>
+        <li>
+            <a href="calendar.html"><i class="icon-chevron-right"></i> Calendar</a>
+        </li>
+
+        <li>
+            <a href="#"><span class="badge badge-success pull-right">731</span> Orders</a>
+        </li>
+
+    </ul>
