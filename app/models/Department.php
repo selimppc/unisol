773875bb3d766e9a606f2ed7603f4186a1602702
ratnaakter <ratnaakter17@gmail.com>
2014-12-17 12:09:31 +0600

    new model

--- app/models/Department.php
+++ app/models/Department.php
@@ -1,8 +1,20 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 12/15/2014
+ * Time: 12:42 PM
+ */
 
-class Department extends \Eloquent{
+class Department extends Eloquent {
 
-    protected $fillable = [];
     protected $table = 'department';
 
-}
+    //get the department name according their id in show.blade.php; this function only return the department name using department id from subject table
+    public static function getDepartmentName($deptId){
+    	$data = Department::find($deptId);    
+    	return $data->title;
+    }
+
+} 
+
