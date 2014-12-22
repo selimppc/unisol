7ba1e57991fcf1877543c3843da1171d20b081cc
tanin <tanintjt@gmail.com>
2014-12-22 15:13:42 +0600

    latest push

--- app/views/subject/_form.blade.php
+++ app/views/subject/_form.blade.php
@@ -7,13 +7,11 @@
     <div class='form-group'>
     <div>{{ Form::label('title', 'SubjectName') }}</div>
     <div>{{ Form::text('title', Input::old('title'),['class'=>'form-control','spellcheck'=> 'true','required'=>'required']) }}</div>
-
     </div>
 
     <div class='form-group'>
-    <div>{{ Form::label('description', 'Description') }}</div>
-    <div>{{ Form::textarea('description', Input::old('description'),['class'=>'form-control','spellcheck'=> 'true','required'=>'required','size'=>'30x10']) }}</div>
-
+       <div>{{ Form::label('description', 'Description') }}</div>
+       <div>{{ Form::textarea('description', Input::old('description'),['class'=>'form-control','spellcheck'=> 'true','required'=>'required','size'=>'30x10']) }}</div>
     </div>
 
     <div>
