d684f0145e4c11b9397a2fa46ff5a65a85e3ee30
shafiqul haque <shafiqulhaque1971@gmail.com>
2014-12-23 16:17:41 +0600

    Issue #16 controller updates

--- app/controllers/RoleTaskController.php
+++ app/controllers/RoleTaskController.php
@@ -1,164 +1,98 @@
 <?php
 
-class RoleTaskUserController extends \BaseController {
+class RoleTaskController extends \BaseController {
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
+
     public function index()
     {
-        $role_task_user = RoleTaskUser::orderBy('id', 'DESC')->paginate(10);
+        $role_task_list = DB::table('role_task')->get();
+        return View::make('role_task.index', compact('role_task_list'));
 
-        return View::make('role_task_user.index')->with('roleTaskUser',$role_task_user);
     }
 
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
     public function create()
     {
-        return View::make('role_task_user.create');
+        //
     }
 
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @return Response
-     */
     public function store()
     {
-        $data = Input::all();
+        $rules = array(
+            'title' => 'required',
 
-        // create a new model instance
-        $role_task_user = new RoleTaskUser();
+        );
 
-        // attempt validation
-        if ($role_task_user->validate($data))
-        {
+        $validator = Validator::make(Input::all(), $rules);
 
-            $role_task_user->role_task_id = Input::get('role_task_id');
-            $role_task_user->user_id = Input::get('user_id');
-            $role_task_user->status = Input::get('status');
+        if ($validator->passes()) {
+            $roletask = new RoleTask;
 
-            $role_task_user->save();
-
-            // redirect
-            Session::flash('message', 'Successfully Added!');
-            return Redirect::to('role_task_user');
-        }
-        else
-        {
-            // failure, get errors
-            $errors = $role_task_user->errors();
-            Session::flash('errors', $errors);
-
-            return Redirect::to('role_task_user/create');
+            $roletask->target_role_id = Input::get('target_role_id');
+            $roletask->task_list_id = Input::get('task_list_id');
+            $roletask->title = Input::get('title');
+            $roletask->description = Input::get('description');
+            $roletask->status = Input::get('status');
+            $roletask->save();
+            // return Redirect::to('crud')->with('message', 'Successfully added Country!');
+            return Redirect::back()->with('message', 'Successfully Added Information!');
+        } else {
+            return Redirect::to('roletask/index')->with('message', 'The following errors occurred')->withErrors($validator)->withInput();
         }
     }
 
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
     public function show($id)
     {
-        $role_task_user = RoleTaskUser::find($id);
+        $roletask = RoleTask::find($id);
 
-        if($role_task_user)
-        {
-            return View::make('role_task_user.show')->with('roleTaskUser',$role_task_user);
-        }
-        App::abort(404);
+        return View::make('role_task.show',compact('roletask'));
     }
 
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
     public function edit($id)
-    {
-        $role_task_user = RoleTaskUser::find($id);
 
+    {
+        $roletask = RoleTask::find($id);
         // Show the edit employee form.
-        return View::make('role_task_user.edit')->with('roleTaskUser',$role_task_user);
+        return View::make('role_task.edit', compact('roletask'));
     }
 
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
     public function update($id)
     {
-        // get the POST data
-        $data = Input::all($id);
-        // create a new model instance
-        $role_task_user = new RoleTaskUser();
-        // attempt validation
-        if ($role_task_user->validate2($data))
-        {
-            // success code
-            $role_task_user = RoleTaskUser::find($id);
+        $rules = array(
+            'title' => 'required',
 
-            $role_task_user->role_task_id = Input::get('role_task_id');
-            $role_task_user->user_id = Input::get('user_id');
-            $role_task_user->status = Input::get('status');
 
-            $role_task_user->save();
+        );
 
-            // redirect
-            Session::flash('message', 'Successfully Edited!');
-            return Redirect::to('role_task_user');
-        }
-        else
-        {
-            // failure, get errors
-            $errors = $role_task_user->errors();
-            Session::flash('errors', $errors);
+        $validator = Validator::make(Input::all(), $rules);
 
-            //return Redirect::to('employee/create')->withInput()->withErrors($errors);
-            return Redirect::to('role_task_user');
+        if ($validator->passes()) {
+
+            $roletask = RoleTask::find($id);
+            $roletask->title = Input::get('title');
+            $roletask->target_role_id = Input::get('target_role_id');
+            $roletask->task_list_id = Input::get('task_list_id');
+            $roletask->status = Input::get('status');
+            $roletask->description = Input::get('description');
+
+            $roletask->save();
+
+            return Redirect::back()->with('message', 'Successfully Added Information!');
+        } else {
+            return Redirect::to('roletask/index')->with('message', 'The following errors occurred')->withErrors($validator)->withInput();
         }
     }
 
+    public function destroy($id){
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-
-        $data= RoleTaskUser::find($id);
-        if($data->delete())
-        {
-            return Redirect::back()->with('message', 'Successfully deleted!');
-        }
-        //ok
+        RoleTask::find($id)->delete();
+        return Redirect::back()->with('message', 'Successfully deleted Information!');
     }
 
-    public function batchDelete(){
 
 
-        RoleTaskUser::destroy(Request::get('id'));
-        return Redirect::back();
-        //ok
-    }
+
 
 }
