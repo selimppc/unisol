ef16d37920c772a2b0179e4326fc1e295008a117
ratnaakter <ratnaakter17@gmail.com>
2014-12-24 17:54:45 +0600

    Issue 14 Last Updated code

--- app/views/year/index.blade.php
+++ app/views/year/index.blade.php
@@ -111,7 +111,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title">Add New Subject</h4>
+                    <h4 class="modal-title">Add New Year</h4>
                 </div>
                 <div class="modal-body">
                    {{ Form::open(array('url' => 'year/save', 'method' =>'post', 'role'=>'form','files'=>'true')) }}
