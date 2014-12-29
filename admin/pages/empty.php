42858cf2400c1111ef75daadf897e49fa45e5615
Selim Reza <me@selimreza.com>
2014-12-29 11:32:46 +0600

    Issues #15 updated admin

---
+++ admin/pages/empty.php
@@ -0,0 +1,28 @@
+<?php include('../_header.php'); ?>
+        <div class="wrapper row-offcanvas row-offcanvas-left">
+            <!-- Left side column. contains the logo and sidebar -->
+            <?php include('../_sidebar.php'); ?>
+
+            <!-- Right side column. Contains the navbar and content of the page -->
+            <aside class="right-side">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1>
+                        Blank page
+                        <small>Control panel</small>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                        <li class="active">Blank page</li>
+                    </ol>
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+
+
+                </section><!-- /.content -->
+            </aside><!-- /.right-side -->
+        </div><!-- ./wrapper -->
+
+<?php include('../_footer.php'); ?>
