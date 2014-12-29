12218ef8073a6b1f25704683a166d6e12017243e
shafiqul haque <shafiqulhaque1971@gmail.com>
2014-12-29 18:10:13 +0600

    ISsue #15 file structure of academic module and nav bar done

---
+++ academic/account/sidebar.php
@@ -0,0 +1,43 @@
+<aside class="left-side sidebar-offcanvas">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="img/avatar3.png" class="img-circle" alt="User Image" />
+            </div>
+            <div class="pull-left info">
+                <p>Hello, Edu Tech</p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+        <!-- search form -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search..."/>
+                            <span class="input-group-btn">
+                                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
+                            </span>
+            </div>
+        </form>
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu">
+            <li class="active">
+                <a href="index.php">
+                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="pages/enrollment.php">
+                    <i class="fa fa-laptop"></i>
+                    <span>Enrollment</span>
+                </a>
+
+            </li>
+
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
