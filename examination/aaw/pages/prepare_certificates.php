407779a7acb6f87c83bf9a8f68ecc0c59e14df1e
Almas Hossain <almas@edutechsolutionsbd.com>
2014-12-31 23:52:25 -0600

    Issue #15 rename exam AAW to exam aaw.

---
+++ examination/aaw/pages/prepare_certificates.php
@@ -0,0 +1,636 @@
+<?php include('../_header.php'); ?>
+
+<div class="wrapper row-offcanvas row-offcanvas-left">
+<!-- Left side column. contains the logo and sidebar -->
+
+<?php include('../_sidebar.php'); ?>
+
+
+<!-- Right side column. Contains the navbar and content of the page -->
+<aside class="right-side">
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        Dashboard
+        <small>Control panel</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+    </ol>
+</section>
+
+
+<!-- Main content -->
+<section class="content">
+<div class="box-body">
+    <p>
+        <button class="pull-right btn btn-primary btn-sm"  data-toggle="modal" data-target="#massage">Send Notification To Student </button>
+    </p>
+    <p>
+        <button class="pull-left btn btn-primary btn-sm"  data-toggle="modal" data-target="#add">Add New </button>
+    </p>
+
+</div>
+<div class="row">
+<div class="col-xs-12">
+
+<div class="box">
+<div class="box-header">
+    <h3 class="box-title">Issue Certificates</h3>
+</div><!-- /.box-header -->
+<div class="box-body table-responsive">
+<table id="example1" class="table table-bordered table-striped">
+<thead>
+<tr>
+
+    <th>Student Name</th>
+    <th>Roll </th>
+    <th>Department</th>
+    <th>Semester</th>
+    <th>Year</th>
+    <th>Certificates</th>
+    <th>Action</th>
+
+</tr>
+</thead>
+<tbody>
+
+<tr>
+
+    <td>Md. Jamal</td>
+
+
+    <td>CE-01001</td>
+    <td>CSE</td>
+    <td>Spring</td>
+    <td>2011</td>
+    <th>
+            <span data-toggle="modal" data-target="#view">
+                <img src="../img/doc.png" width="50px" style="cursor: pointer">
+            </span>
+    </th>
+
+
+    <th>
+        <button class="btn btn-success btn-sm" data-toggle="modal" data-target="#print">Print</button>
+
+        </span>
+    </th>
+
+</tr>
+<tr>
+
+    <td>Md. Jamal</td>
+
+
+    <td>CE-01001</td>
+    <td>CSE</td>
+    <td>Spring</td>
+    <td>2011</td>
+    <th>
+            <span data-toggle="modal" data-target="#view">
+                <img src="../img/doc.png" width="50px" style="cursor: pointer">
+            </span>
+    </th>
+
+
+    <th>
+        <button class="btn btn-success btn-sm" data-toggle="modal" data-target="#print">Print</button>
+
+        </span>
+    </th>
+
+</tr>
+<tr>
+
+    <td>Md. Jamal</td>
+
+
+    <td>CE-01001</td>
+    <td>CSE</td>
+    <td>Spring</td>
+    <td>2011</td>
+    <th>
+            <span data-toggle="modal" data-target="#view">
+                <img src="../img/doc.png" width="50px" style="cursor: pointer">
+            </span>
+    </th>
+
+
+    <th>
+        <button class="btn btn-success btn-sm" data-toggle="modal" data-target="#print">Print</button>
+
+        </span>
+    </th>
+
+</tr>
+<tr>
+
+    <td>Md. Jamal</td>
+
+
+    <td>CE-01001</td>
+    <td>CSE</td>
+    <td>Spring</td>
+    <td>2011</td>
+    <th>
+            <span data-toggle="modal" data-target="#view">
+                <img src="../img/doc.png" width="50px" style="cursor: pointer">
+            </span>
+    </th>
+
+
+    <th>
+        <button class="btn btn-success btn-sm" data-toggle="modal" data-target="#print">Print</button>
+
+        </span>
+    </th>
+
+</tr>
+<tr>
+
+    <td>Md. Jamal</td>
+
+
+    <td>CE-01001</td>
+    <td>CSE</td>
+    <td>Spring</td>
+    <td>2011</td>
+    <th>
+            <span data-toggle="modal" data-target="#view">
+                <img src="../img/doc.png" width="50px" style="cursor: pointer">
+            </span>
+    </th>
+
+
+    <th>
+        <button class="btn btn-success btn-sm" data-toggle="modal" data-target="#print">Print</button>
+
+        </span>
+    </th>
+
+</tr>
+<tr>
+
+    <td>Md. Jamal</td>
+
+
+    <td>CE-01001</td>
+    <td>CSE</td>
+    <td>Spring</td>
+    <td>2011</td>
+    <th>
+            <span data-toggle="modal" data-target="#view">
+                <img src="../img/doc.png" width="50px" style="cursor: pointer">
+            </span>
+    </th>
+
+
+    <th>
+        <button class="btn btn-success btn-sm" data-toggle="modal" data-target="#print">Print</button>
+
+        </span>
+    </th>
+
+</tr>
+<tr>
+
+    <td>Md. Jamal</td>
+
+
+    <td>CE-01001</td>
+    <td>CSE</td>
+    <td>Spring</td>
+    <td>2011</td>
+    <th>
+            <span data-toggle="modal" data-target="#view">
+                <img src="../img/doc.png" width="50px" style="cursor: pointer">
+            </span>
+    </th>
+
+
+    <th>
+        <button class="btn btn-success btn-sm" data-toggle="modal" data-target="#print">Print</button>
+
+        </span>
+    </th>
+
+</tr>
+<tr>
+
+    <td>Md. Jamal</td>
+
+
+    <td>CE-01001</td>
+    <td>CSE</td>
+    <td>Spring</td>
+    <td>2011</td>
+    <th>
+            <span data-toggle="modal" data-target="#view">
+                <img src="../img/doc.png" width="50px" style="cursor: pointer">
+            </span>
+    </th>
+
+
+    <th>
+        <button class="btn btn-success btn-sm" data-toggle="modal" data-target="#print">Print</button>
+
+        </span>
+    </th>
+
+</tr>
+<tr>
+
+    <td>Md. Jamal</td>
+
+
+    <td>CE-01001</td>
+    <td>CSE</td>
+    <td>Spring</td>
+    <td>2011</td>
+    <th>
+            <span data-toggle="modal" data-target="#view">
+                <img src="../img/doc.png" width="50px" style="cursor: pointer">
+            </span>
+    </th>
+
+
+    <th>
+        <button class="btn btn-success btn-sm" data-toggle="modal" data-target="#print">Print</button>
+
+        </span>
+    </th>
+
+</tr>
+<tr>
+
+    <td>Md. Jamal</td>
+
+
+    <td>CE-01001</td>
+    <td>CSE</td>
+    <td>Spring</td>
+    <td>2011</td>
+    <th>
+            <span data-toggle="modal" data-target="#view">
+                <img src="../img/doc.png" width="50px" style="cursor: pointer">
+            </span>
+    </th>
+
+
+    <th>
+        <button class="btn btn-success btn-sm" data-toggle="modal" data-target="#print">Print</button>
+
+        </span>
+    </th>
+
+</tr>
+<tr>
+
+    <td>Md. Jamal</td>
+
+
+    <td>CE-01001</td>
+    <td>CSE</td>
+    <td>Spring</td>
+    <td>2011</td>
+    <th>
+            <span data-toggle="modal" data-target="#view">
+                <img src="../img/doc.png" width="50px" style="cursor: pointer">
+            </span>
+    </th>
+
+
+    <th>
+        <button class="btn btn-success btn-sm" data-toggle="modal" data-target="#print">Print</button>
+
+        </span>
+    </th>
+
+</tr>
+
+
+
+
+</tbody>
+<tfoot>
+<tr>
+
+    <th>Department</th>
+
+    <th>Semester </th>
+    <th>Year</th>
+    <th>Subject</th>
+    <th>Examination Date</th>
+    <th>Certificates</th>
+    <th>Action</th>
+</tr>
+</tfoot>
+</table>
+</div><!-- /.box-body -->
+</div><!-- /.box -->
+</div>
+</div>
+
+
+
+
+</section><!-- /.content -->
+</aside><!-- /.right-side -->
+</div><!-- ./wrapper -->
+
+<!-- Modal :: add-->
+
+
+<div class="modal fade " id="add" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Add Result</h4>
+            </div>
+            <div class="modal-body">
+
+
+                <form role="form">
+                    <div class="box-body">
+
+                        <div class="form-group">
+                            <label for="name">Student Name</label>
+                            <input type="name" class="form-control" id="name" >
+                        </div>
+
+                        <div class="form-group">
+                            <label for="roll">Student Roll</label>
+                            <input type="roll" class="form-control" id="roll" >
+                        </div>
+
+                        <div class="form-group">
+                            <label for="department">Department</label>
+                            <select class="form-control input-sm" id="department">
+                                <option>Select a Department</option>
+                                <option value="CSE">CSE</option>
+                                <option value="ICT">ICT</option>
+                                <option value="ENGLISH">ENGLISH</option>
+                                <option value="PHYSICS">PHYSICS</option>
+                                <option value="CSE">MATH</option>
+                                <option value="CSE">BBA</option>
+                                <option value="CSE">MBA</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="semester">Semester</label>
+                            <select class="form-control input-sm" id="semester">
+                                <option>Select Semester</option>
+                                <option value="CSE">Spring</option>
+                                <option value="ICT">Summer</option>
+                                <option value="ENGLISH">Fall</option>
+                            </select>
+                        </div>
+
+
+
+                        <div class="form-group">
+                            <label for="year">Year</label>
+                            <select class="form-control input-sm" id="year">
+                                <option>Select Year</option>
+                                <option value="CSE">2010</option>
+                                <option value="ICT">2011</option>
+                                <option value="ENGLISH">2012</option>
+                                <option value="ENGLISH">2013</option>
+                                <option value="ENGLISH">2014</option>
+                                <option value="ENGLISH">2015</option>
+                                <option value="ENGLISH">2016</option>
+
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="upload">upload Certificate</label>
+                            <input type="file" class="form-control" id="upload" >
+                        </div>
+
+
+
+
+                    </div>
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-primary">Save</button>
+                        <button type="button" class="btn btn-success" data-dismiss="modal">Cancel</button>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- Modal :: view  -->
+
+<div class="modal fade " id="view" tabindex="-1" role="dialog" aria-labelledby="confirm-delete" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel"></h4>
+            </div>
+            <div class="modal-body">
+                <div style="padding: 20px;">
+                    <table>
+                        <h4>Information</h4>
+                        <tr
+
+                        <td>Name:Md. Jamal</td></tr>
+
+
+                        <td>Roll:CE-01001</td></tr>
+                        <td>Department:CSE</td></tr>
+                        <td>Semester:Spring</td></tr>
+                        <td>Year:2011</td></tr>
+
+
+                        <td> Certificate:  <img src="../img/doc.png" width="50px" style="cursor: pointer"></td>
+
+
+
+                        </tr>
+                    </table>
+
+
+                </div>
+            </div>
+            <div class="modal-footer">
+
+
+                <button type="submit" class="btn btn-info" data-dismiss="modal">Cancel</button>
+
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+<!-- View Details Modal -->
+<div class="modal fade" id="print" tabindex="-1" role="dialog" aria-labelledby="viewDetails" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title">Print Certificate</h4>
+            </div>
+            <div class="modal-body">
+
+
+                <p>
+                    <img src="../img/doc.png" >
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-info" data-dismiss="modal">Print</button>
+
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+<!--send notification:Modal-->
+
+<div class="modal fade" id="massage" tabindex="-1" role="dialog" aria-labelledby="viewDetails" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title">Send Notification</h4>
+            </div>
+            <div class="modal-body">
+
+                <form role="form">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="name">Student Name</label>
+                            <input type="name" class="form-control" id="name" >
+                        </div>
+
+
+
+
+                        <div class="form-group">
+                            <label for="department">Department</label>
+                            <select class="form-control input-sm" id="department">
+                                <option>Select a Department</option>
+                                <option value="CSE">CSE</option>
+                                <option value="ICT">ICT</option>
+                                <option value="ENGLISH">ENGLISH</option>
+                                <option value="PHYSICS">PHYSICS</option>
+                                <option value="CSE">MATH</option>
+                                <option value="CSE">BBA</option>
+                                <option value="CSE">MBA</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="semester">Semester</label>
+                            <select class="form-control input-sm" id="semester">
+                                <option>Select Semester</option>
+                                <option value="CSE">Spring</option>
+                                <option value="ICT">Summer</option>
+                                <option value="ENGLISH">Fall</option>
+                            </select>
+                        </div>
+
+
+
+                        <div class="form-group">
+                            <label for="year">Year</label>
+                            <select class="form-control input-sm" id="year">
+                                <option>Select Year</option>
+                                <option value="CSE">2010</option>
+                                <option value="ICT">2011</option>
+                                <option value="ENGLISH">2012</option>
+                                <option value="ENGLISH">2013</option>
+                                <option value="ENGLISH">2014</option>
+                                <option value="ENGLISH">2015</option>
+                                <option value="ENGLISH">2016</option>
+
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email Address</label>
+                            <input type="email" class="form-control" id="email" >
+                        </div>
+
+                        <div class="form-group">
+                            <label for="msg">Massage</label>
+                            <input type="msg" class="form-control" id="msg" value="Your Certificate is ready,Please receive it from your department">
+                        </div>
+
+                    </div>
+                </form>
+
+            </div>
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-info" data-dismiss="modal"  data-toggle="modal" data-target="#send_msg">Send</button>
+
+
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<!--get msg:modal-->
+
+<div class="modal fade" id="send_msg" tabindex="-1" role="dialog" aria-labelledby="viewDetails" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title">Send Notification</h4>
+            </div>
+            <div class="modal-body">
+
+                <form role="form">
+                    <div class="box-body">
+                        <p>The notification has been  sent to all of the students of <b>'CSE'</b> Department</p>
+
+                    </div>
+                </form>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+
+<script type="text/javascript">
+    $(function() {
+        $("#example1").dataTable();
+        $('#example2').dataTable({
+            "bPaginate": true,
+            "bLengthChange": false,
+            "bFilter": false,
+            "bSort": true,
+            "bInfo": true,
+            "bAutoWidth": false
+        });
+    });
+</script>
+
+<?php include('../_footer.php'); ?>
