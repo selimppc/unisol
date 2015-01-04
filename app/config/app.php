65898c1fe761efebde0738699b54fba83534b13f
Selim Reza <me@selimreza.com>
2015-01-04 17:05:52 +0600

    Module Configuration

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,8 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
         'App\Modules\Common\ServiceProvider',
+        'App\Modules\Academic\ServiceProvider',
+        'App\Modules\Admission\ServiceProvider',
 
 	),
 
