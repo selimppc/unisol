04978e462a3b9589392a355b5ef934d08af07b9e
tanin <tanintjt@gmail.com>
2015-01-08 10:39:28 +0600

    Issue # 16 /views/admission/Signup/index.blade.php updated

--- app/modules/admission/views/admission/Signup/index.blade.php
+++ app/modules/admission/views/admission/Signup/index.blade.php
@@ -20,13 +20,13 @@
 
 
         {{ Form::label('middlename', 'Middle Name:') }}
-        {{ Form::text('middlename',Input::old('middlename'), array('class' => 'form-control','placeholder'=>'Enter your middle name')) }}
+        {{ Form::text('middlename',Input::old('middlename'), array('class' => 'form-control')) }}
 
         {{ Form::label('lastname', 'Last Name:') }}
         {{ Form::text('lastname',Input::old('lastname'), array('class' => 'form-control','placeholder'=>'Enter your last name')) }}
 
         {{ Form::label('email', 'Email') }}
-        {{ Form::text('email', Input::old('email'), array('class'=>'form-control','required'=>'required')) }}
+        {{ Form::text('email', Input::old('email'), array('class'=>'form-control','required'=>'required','placeholder'=>'Enter a valid email address')) }}
 
         {{ Form::label('username', 'User Name:') }}
         {{ Form::text('username',Input::old('username'), array('class' => 'form-control','placeholder'=>'Enter your user name')) }}
@@ -34,8 +34,11 @@
         {{ Form::label('password', 'Password') }}
         {{ Form::password('password', array('placeholder'=>'........','class'=>'form-control','required'=>'required')) }}
 
+        {{ Form::label('confirmpassword', 'ConfirmPassword') }}
+        {{ Form::password('confirmpassword', array('placeholder'=>'........','class'=>'form-control','required'=>'required')) }}
+
         {{ Form::label('targetrole', 'Target Role') }}
-        {{ Form::select('targetrole', array(''=>'Select','applicant' => 'Applicant', 'teacher' => 'Teacher','staff'=>'Staff','alumni'=>'Alumni','employer' => 'Employer'), '', array('class' => 'form-control','required'=>'required'))}}
+        {{ Form::select('targetrole', array(''=>'Select One','applicant' => 'Applicant', 'teacher' => 'Teacher','staff'=>'Staff','alumni'=>'Alumni','employer' => 'Employer'), '', array('class' => 'form-control','required'=>'required'))}}
           <br>
 
         {{--{{ HTML::image(Captcha::img(), 'Captha image') }}--}}
@@ -59,4 +62,16 @@
 
 </div>
 
+
+<script type="text/javascript">
+ $(document).ready(function() {
+        $('#firstname').tooltip({  title: 'Write your  firstname.This is a required field',placement : 'right' });
+        $('#middlename').tooltip({  title: 'Write your  Middlename.(Optional)',placement : 'right' });
+        $('#lastname').tooltip({  title: 'Write your lastname.This is a required field',placement : 'right' });
+        $('#email').tooltip({  title: 'Write your email address here to varify your account',placement : 'right' });
+        $('#username').tooltip({  title: 'Write your  username here to access your account',placement : 'right' });
+        $('#password').tooltip({  title: 'Your Password should be at least 8 charecters.1 uppercase,1 lowercase,1 number,1 special charecter is must',placement : 'right' })
+        $('#confirmpassword').tooltip({  title: 'Write again your password  to confirm it',placement : 'right' });
+});
+</script>
 @stop
