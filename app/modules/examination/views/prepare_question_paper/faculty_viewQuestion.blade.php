c4dbd0d7ffc8e876316738a61488229e3c3ab84c
tanin <tanintjt@gmail.com>
2015-01-20 16:59:30 +0600

    oops

---
+++ app/modules/examination/views/prepare_question_paper/faculty_viewQuestion.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.master')
+
+@section('sidebar')
+    @include('examination::prepare_question_paper._sidebar')
+@stop
+
+@section('content')
+
+
+             <h1>Welcome to View Question : Faculty</h1> <br>
+
+
+              <table id="example" class="table table-striped  table-bordered"  >
+                     <thead>
+
+                           <div class="btn-group" style="margin-right: 10px">
+                              <a class="btn btn-default" data-toggle="modal" data-target="#AddQuestionModal">Add Question</a>
+                           </div>
+
+                           {{--page content start from here--}}
+                          <br>
+                               {{ Form::submit('Delete Items', array('class'=>'btn btn-danger', 'id'=>'hide-button', 'style'=>'display:none'))}}
+                          <br>
+
+
+                         <tr>
+                             <th><input name="id" type="checkbox" id="checkbox" class="checkbox" value=""></th>
+
+                             <th>Title</th>
+                             <th>Question Type</th>
+                             <th>Answer Type</th>
+                             <th>Action</th>
+                         </tr>
+                     </thead>
+                     <tbody>
+
+                       {{--@foreach($prepareQuestionPaper as $prep_quest_paper)--}}
+                         {{--<tr>--}}
+
+                             {{--<td><input type="checkbox" name="id[]"  id="checkbox" class="myCheckbox" value="  {{ $prep_quest_paper->id }}"></td>--}}
+
+                             {{--<td>a</td>--}}
+                             {{--<td>a</td>--}}
+                             {{--<td>a</td>--}}
+                            {{--<td>--}}
+                              {{--<a type="button" class="btn btn-info" data-toggle="modal" data-target="#EditQuestionPaperModal"> Edit Question Papers </a>--}}
+
+
+                            {{--</td>--}}
+
+                         {{--</tr>--}}
+                             {{--@endforeach--}}
+
+                     </tbody>
+              </table>
+
+
+
+@include('examination::prepare_question_paper/_modal');
+@stop
+
