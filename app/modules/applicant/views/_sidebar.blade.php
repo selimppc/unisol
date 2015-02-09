42f9d16d77d3be8c81a43aa1bd52c5e1efa69b35
tanin <tanintjt@gmail.com>
2015-02-09 12:28:13 +0600

    bug fixing of applicant's sdoc_info, profile

--- app/modules/applicant/views/_sidebar.blade.php
+++ app/modules/applicant/views/_sidebar.blade.php
@@ -1,12 +1,10 @@
 <p class="lead">Quick Navigation</p>
 <div class="list-group">
-    <a href= "{{URL::to('applicant/profile/index') }}" class="list-group-item">Profile</a>
-    <a href="{{URL::to('applicant/profile') }}" class="list-group-item">Academic Records</a>
-    {{--<a href="{{URL::to('applicant/meta_data') }}" class="list-group-item">Biographical</a>--}}
-    <a href="{{URL::to('applicant/personal_info/index') }}" class="list-group-item">Personal Information</a>
-    <a href="{{URL::to('applicant/supporting_docs/index') }}" class="list-group-item">Supporting Documents</a>
-    <a href="{{URL::to('applicant/extra_curricular') }}" class="list-group-item active">Extra-curricular Activities</a>
-
-    <a href="{{URL::to('applicant/miscellaneous_info/index') }}" class="list-group-item">Miscellaneous Information</a>
+    <a href= "{{URL::to('applicant/profile/index') }}"  class="list-group-item">Profile</a>
+    <a href="{{URL::to('applicant/profile') }}" class="list-group-item ">Academic Records</a>
+    <a href="{{URL::to('applicant/personal_info/index') }}" class="list-group-item ">Personal Information</a>
+    <a href="{{URL::to('applicant/supporting_docs/index') }}" class="list-group-item ">Supporting Documents</a>
+    <a href="{{URL::to('applicant/extra_curricular') }}" class="list-group-item ">Extra-curricular Activities</a>
+    <a href="{{URL::to('applicant/miscellaneous_info/index') }}" class="list-group-item ">Miscellaneous Information</a>
 
 </div>
