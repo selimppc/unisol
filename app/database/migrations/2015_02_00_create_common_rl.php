cb53b867a4b41cf03f6873f822f4c1ab10233d3a
Selim Reza <me@selimreza.com>
2015-02-10 15:05:34 +0600

    common migration

--- app/database/migrations/2015_02_00_create_common_rl.php
+++ app/database/migrations/2015_02_00_create_common_rl.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCommonRl extends Migration {
+class CommonRl extends Migration {
 
 	public function up()
 	{
