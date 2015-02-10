e26ae7ea2d17296fce076b0f493a6780fe463cd9
ratnaakter <ratnaakter17@gmail.com>
2015-02-10 17:37:21 +0600

    Issue #36 academic module faculty marks distribution update code

--- app/modules/academic/models/AcmMarksDistribution.php
+++ app/modules/academic/models/AcmMarksDistribution.php
@@ -4,4 +4,16 @@ class AcmMarksDistribution extends \Eloquent
     protected $fillable = [];
     protected $table = 'acm_marks_distribution';
 
+
+    public function relCourseManagement()
+    {
+        return $this->belongsTo('CourseManagement', 'course_management_id', 'id');
+    }
+    public function relAcmMarksDistItem(){
+        return $this->belongsTo('AcmMarksDist', 'acm_marks_dist_item_id', 'id');
+    }
+    public function relAcmMarksPolicy(){
+        return $this->belongsTo('AcmMarksPolicy', 'acm_marks_policy_id', 'id');
+    }
+
 }
--- app/modules/academic/views/mark_distribution_courses/teacher/show.blade.php
+++ app/modules/academic/views/mark_distribution_courses/teacher/show.blade.php
@@ -14,7 +14,7 @@
             </thead>
             <tbody>
 
-            @foreach($datas as $value)
+            @foreach($data as $value)
                 <tr>
                     <td>{{$value['relCourse']['title']}}</td>
                     <td>{{$value->relCourse->relSubject->relDepartment->title}}</td>
@@ -26,30 +26,29 @@
             </tbody>
         </table>
         <p>Following is the MarksDistribution of this course.</p>
-        {{--<p> Total Marks: {{ $datas['relCourse']['evaluation_total_marks']}}</p>--}}
 
-        {{--<table class="table table-bordered">--}}
-            {{--<thead>--}}
-            {{--<th>Item</th>--}}
+
+        <table class="table table-bordered">
+            <thead>
+            <th>Item</th>
             {{--<th>Marks(%)</th>--}}
-            {{--<th>Actual Marks</th>--}}
-            {{--<th>Course</th>--}}
-            {{--<th>IsAttendance</th>--}}
-            {{--</thead>--}}
-            {{--<tbody>--}}
-            {{--@foreach($config_data as  $dkey => $dvalue)--}}
-                {{--<p>Total Marks:</p>--}}
-                {{--<p>{{$dvalue['relCourse']['evaluation_total_marks']}}</p>--}}
-                {{--<tr>--}}
-                    {{--<td>{{$dvalue['relAcmMarksDistItem']['title']}}</td>--}}
+            <th>Actual Marks</th>
+            <th>Policy</th>
+            <th>IsAttendance</th>
+            </thead>
+            <tbody>
+            @foreach($config_data as  $dkey => $dvalue)
+
+                <tr>
+                    <td>{{$dvalue['relAcmMarksDistItem']['title']}}</td>
                     {{--<td>{{(($dvalue->marks * 100)/$dvalue['relCourse']['evaluation_total_marks'])}}</td>--}}
-                    {{--<td>{{$dvalue->marks}}</td>--}}
-                    {{--<td>{{$dvalue['relCourse']['title']}}</td>--}}
-                    {{--<td>{{($dvalue->is_attendance == 1) ? 'Yes' : '';}}</td>--}}
-                {{--</tr>--}}
-            {{--@endforeach--}}
-            {{--</tbody>--}}
-        {{--</table>--}}
+                    <td>{{$dvalue->marks}}</td>
+                    <td>{{$dvalue['relAcmMarksPolicy']['title']}}</td>
+                    <td>{{($dvalue->is_attendance == 1) ? 'Yes' : '';}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
         <p>If Marks Distribution is not done then go to distribution and make it done first.</p>
     </div>
 </div>
