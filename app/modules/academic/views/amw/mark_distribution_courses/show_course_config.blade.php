d95eaa0e843d88bdf5b20c04ba7cbc346d3c2a4c
ratnaakter <ratnaakter17@gmail.com>
2015-02-17 16:56:42 +0600

    academic module amw updated code for course link view page

--- app/modules/academic/views/amw/mark_distribution_courses/show_course_config.blade.php
+++ app/modules/academic/views/amw/mark_distribution_courses/show_course_config.blade.php
@@ -1,9 +1,8 @@
-<div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-    <h4 class="modal-title">Show Course Config Information</h4>
-</div>
-<div class="modal-body">
-    <div style="padding: 10px; width: 90%;">
+@extends('layouts.master')
+@section('sidebar')
+    @include('academic::_sidebar')
+@stop
+@section('content')
 
         <table class="table table-bordered">
             <thead>
@@ -50,9 +49,11 @@
             @endforeach
             </tbody>
         </table>
-        <p>If Marks Distribution is not done then go to distribution and make it done first.</p>
+        <p>If Marks Distribution is not done then go to distribution and make it done first.<a href="{{URL::to('academic/amw/config/')}}" class="btn btn-link">MarksDist</a>
+        </p>
   </div>
 </div>
 <div class="modal-footer">
-    <a href="{{URL::to('academic/amw/config/')}}" class="btn btn-default">Close </a>
+    <a href="{{URL::to('academic/amw/config/')}}" class="btn btn-default">Back</a>
 </div>
+@stop
