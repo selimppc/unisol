dc9624bb4d8bf1701039701d60bcd4ed9ef4f49f
shafiqul haque <shafiqulhaque1971@gmail.com>
2015-02-23 12:38:38 +0600

    Issue#46 updates

--- app/modules/examination/views/amw/prepare_question_paper/_addExamination_form.blade.php
+++ app/modules/examination/views/amw/prepare_question_paper/_addExamination_form.blade.php
@@ -1,25 +1,19 @@
 <fieldset style="padding: 10px; width: 90%;">
-
-
             <?php
                 $year_id = Year::lists('title', 'id');
                 $semester_id = Semester::lists('title', 'id');
                 $exam_type = AcmMarksDistItem::lists('title','id');
                 $course_name = Course::lists('title','id');
-
             ?>
-
 
                 <div class="form-group">
                        {{ Form::label('title', 'Title') }}
                        {{ Form::text('title', Input::old('title'), array('class' => 'form-control','required'=>'required')) }}
                 </div>
-
                 <div class="form-group">
                         {{ Form::label('exam_type', 'Exam Type') }}
                         {{ Form::select('acm_marks_dist_item_id',$exam_type, Input::old('acm_marks_dist_item_id'), array('class' => 'form-control','required'=>'required')) }}
                 </div>
-
                 <div class="form-group">
                         {{ Form::label('year', 'Year') }}
                         {{ Form::select('year',$year_id, Input::old('year'), array('class' => 'form-control','required'=>'required') ) }}
@@ -28,13 +22,10 @@
                          {{ Form::label('semester', 'Semester') }}
                          {{ Form::select('semester',$semester_id, Input::old('semester'), array('class' => 'form-control','required'=>'required')) }}
                 </div>
-
                 <div class="form-group">
                          {{ Form::label('course_name', 'Course Name') }}
                          {{ Form::select('course_management_id', $course_name, Input::old('semester'), array('class' => 'form-control','required'=>'required')) }}
                 </div>
-
-
             {{ Form::submit('Submit', array('class' => 'btn btn-primary')) }}
-            <a href="{{URL::previous()}}" class="btn btn-default">Close </a>
+             <a href="{{URL::to('examination/amw/examination')}}" class="btn btn-default">Close </a>
 </fieldset>
--- app/modules/examination/views/amw/prepare_question_paper/editQuestionPaper.blade.php
+++ app/modules/examination/views/amw/prepare_question_paper/editQuestionPaper.blade.php
@@ -1,7 +1,7 @@
 <div style="padding: 10px; width: 90%;">
         <h2>Edit Prepare Question Paper : <strong>{{ ucwords(Auth::user()->username) }}</strong></h2>
             {{ Form::model($prepare_question_paper,array('url'=> array('examination/amw/updateQuestionPaper',$prepare_question_paper->id), 'method' => 'POST')) }}
-{{--                  {{ Form::hidden('exam_list_id',$exam_list_id, Input::old('exam_list_id')) }}--}}
+                  {{ Form::hidden('exam_list_id',$exam_list_id, Input::old('exam_list_id')) }}
                       @include('examination::amw.prepare_question_paper._form')
             {{ Form::close() }}
 </div>
