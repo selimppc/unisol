40306de8fba23d503e8f31d58d42ce6b6cda7e8e
tanin <tanintjt@gmail.com>
2015-02-25 11:30:05 +0600

    add date-picker in custom.js file

--- app/modules/admission/views/amw/degree_management/degree_modals/add_waiver_const.blade.php
+++ app/modules/admission/views/amw/degree_management/degree_modals/add_waiver_const.blade.php
@@ -11,12 +11,12 @@
 
           {{Form::hidden('degree_waiver_id', $degree_waiver_id)}}
           {{ Form::label('start_date', 'Start Time') }}
-          {{ Form::text('start_date', Input::old('start_date'),['class'=>'form-control datepicker','required'=>'required']) }}
+          {{ Form::text('start_date', Input::old('start_date'),['class'=>'form-control date_picker','required'=>'required']) }}
 
           <p>&nbsp;</p>
 
           {{ Form::label('end_date', 'End Time') }}
-          {{ Form::text('end_date', Input::old('end_date'),['class'=>'form-control datepicker','required'=>'required'])  }}
+          {{ Form::text('end_date', Input::old('end_date'),['class'=>'form-control date_picker','required'=>'required'])  }}
 
            <p>&nbsp;</p>
 
@@ -33,64 +33,5 @@
     </div>
 
 </div>
-{{--<script>--}}
-{{--$('.datepicker').datepicker({--}}
-    {{--format: 'yyyy/mm/dd'--}}
 
-{{--});--}}
-{{--</script>--}}
-
-    {{--$('.datepicker').datepicker().on('changeDate', function (ev) {--}}
-
-
-        {{--var $picker = $(this);--}}
-        {{--$picker.datepicker();--}}
-        {{--$picker.datepicker('hide');--}}
-
-
-{{--});--}}
-
-{{--</script>--}}
-{{--<script>--}}
-
-{{--$('.datepicker').each(function() {--}}
-{{--//    var minDate = new Date();--}}
-{{--//    minDate.setYear(0000);--}}
-{{--//    minDate.setMonth(00);--}}
-{{--//    minDate.setDate(00);--}}
-
-    {{--var $picker = $(this);--}}
-    {{--$picker.datepicker();--}}
-
-    {{--var pickerObject = $picker.data('datepicker');--}}
-
-    {{--$picker.on('changeDate', function(ev){--}}
-        {{--$picker.datepicker('hide');--}}
-    {{--});--}}
-{{--});--}}
-
-{{--</script>--}}
-
-
-<script>
-//$(document).ready(function(){
-
-        $('.datepicker').datepicker({
-            format: 'yyyy/mm/dd'
-
-        });
-
-        $('.datepicker').each(function() {
-
-            var $picker = $(this);
-            $picker.datepicker();
-
-            var pickerObject = $picker.data('datepicker');
-
-            $picker.on('changeDate', function(ev){
-                $picker.datepicker('hide');
-            });
-        });
-
-//});
-</script>
+{{ HTML::script('assets/js/custom.js')}}
