4217d73b3c089cfeabf22f91ecd502d668640107
shafiqul haque <shafi@edutechsolutionsbd.com>
2015-03-02 10:42:40 +0600

    Issue#51 updates

--- app/modules/examination/views/amw/prepare_question_paper/_modal/_assign_faculty.blade.php
+++ app/modules/examination/views/amw/prepare_question_paper/_modal/_assign_faculty.blade.php
@@ -6,13 +6,10 @@
                 <h4 class="modal-title" id="myModalLabel">Assign Facutly</h4>
             </div>
             <div class="modal-body">
-
-                     {{ Form::open(array('url' => 'examination/amw/assignFaculty', 'method' =>'post', 'role'=>'form','files'=>'true'))  }}
-                            {{--{{ Form::text('exm_exam_list_id', 3 ) }}--}}
+                     {{ Form::open(array('url' => 'examination/amw/assign_faculty', 'method' =>'post', 'role'=>'form','files'=>'true'))  }}
+                            {{ Form::text('exm_exam_list_id', $exam_list_id ) }}
                             @include('examination::amw.prepare_question_paper._assignFaculty_form')
                     {{ Form::close() }}
-
-
             </div>
             <div class="modal-footer">
             </div>
