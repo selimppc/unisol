fd32efb006c3d321735ff77061ae1c16cd930deb
Selim Reza <selim@edutechsolutionsbd.com>
2015-03-09 12:10:30 +0600

    new layout design for application

--- app/views/layouts/user_access.blade.php
+++ app/views/layouts/user_access.blade.php
@@ -1,41 +1,39 @@
-@extends('layouts.master')
+@extends('layouts.layout')
 @section('content')
 
     <script>
         $(document).ready(function(){
             //$('#CreateModal').modal('show');
-            $('#alertModal').modal('show');
+            $('#user-access').modal('show');
         });
     </script>
 
-    <!-- Modal for Create -->
-     <div data-backdrop="static" data-keyboard="false" class="modal fade" id="alertModal" tabindex="-1" role="dialog" aria-labelledby="showingModal" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
+
+     <!-- User - Access Modal -->
+     <div data-backdrop="static" data-keyboard="false" class="modal fade" id="user-access" tabindex="-1" role="dialog" aria-labelledby="addCategory" aria-hidden="true">
+         <div class="modal-dialog">
+             <div class="modal-content">
                  <div class="modal-header">
-                    <span class="close_button">
-                        <a href="{{URL::previous()}}" class="btn btn-info ">
-                            <span aria-hidden="true">&times;</span>
-                        </a>
-                    </span>
-                    <h4 class="modal-title" id="myModalLabel">
-                        You are: <span style="font-weight: bold; color: #002a80;">
-                                    {{isset(Auth::user()->get()->id) ? Auth::user()->get()->username : Auth::applicant()->get()->username}}
-                                </span>
-                    </h4>
+                     <span class="close_button" style="float: right">
+                         <a href="{{URL::previous()}}" class="btn btn-info "> <span aria-hidden="true">&times;</span> </a>
+                     </span>
+                     <h4 class="modal-title" id="myModalLabel">
+                         You are: <span style="font-weight: bold; color: #002a80;">
+                           {{isset(Auth::user()->get()->id) ? Auth::user()->get()->username : Auth::applicant()->get()->username}}
+                           </span>
+                     </h4>
                  </div>
                  <div class="modal-body">
-                         <h3 style="color: #ad6704;">You are not authorized to perform this action! </h3>
-                         <div>
-                            {{HTML::image('img/forbidden/forbidden_anime.png')}}
-                         </div>
-
+                      <h3 style="color: #ad6704;">You are not authorized to perform this action! </h3>
+                      <div>
+                         {{HTML::image('img/forbidden/forbidden_anime.png')}}
+                      </div>
                  </div>
                  <div class="modal-footer">
                      <a href="{{URL::previous()}}" class="btn btn-primary "> < Go Back </a>
                  </div>
-            </div>
-        </div>
-     </div>
+             </div><!-- /.modal-content -->
+         </div><!-- /.modal-dialog -->
+     </div><!-- /.modal -->
 
 @stop
