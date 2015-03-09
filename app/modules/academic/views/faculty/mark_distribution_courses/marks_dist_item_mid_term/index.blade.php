5f991a5502f4bb32eca252a39fcca117af58c342
ratnaakter <ratna@edutechsolutionsbd.com>
2015-03-09 16:13:35 +0600

    Issue #57 marks dist item mid term update code

--- app/modules/academic/views/faculty/mark_distribution_courses/marks_dist_item_mid_term/index.blade.php
+++ app/modules/academic/views/faculty/mark_distribution_courses/marks_dist_item_mid_term/index.blade.php
@@ -40,7 +40,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" style="text-align: center">Marks Distribution Item:Mid Term</h4>
+                    <h4 class="modal-title"style="text-align: center ;color: #800080">Marks Distribution Item:Mid Term</h4>
                 </div>
                 <div class="modal-body">
                     {{ Form::open(array('url' => 'midterm/save', 'method' =>'post', 'role'=>'form','files'=>'true')) }}
