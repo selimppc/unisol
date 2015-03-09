26961e120ec24118d9481a015dd5a7622c293bca
ratnaakter <ratna@edutechsolutionsbd.com>
2015-03-09 16:03:30 +0600

    Issue #56 marks dist item assignment update code

--- app/modules/academic/views/faculty/mark_distribution_courses/marks_dist_item_assignment/index.blade.php
+++ app/modules/academic/views/faculty/mark_distribution_courses/marks_dist_item_assignment/index.blade.php
@@ -39,7 +39,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" style="text-align: center">Marks Distribution Item:Assignment</h4>
+                    <h4 class="modal-title"style="text-align: center ;color: #800080">Marks Distribution Item:Assignment</h4>
                 </div>
                 <div class="modal-body">
                     {{ Form::open(array('url' => 'assignment/save', 'method' =>'post', 'role'=>'form','files'=>'true')) }}
