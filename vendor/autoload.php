8f6812d161e18809d0ae43c60dee66ef27c67a47
Selim Reza <selim@edutechsolutionsbd.com>
2015-03-10 12:50:35 +0600

    vendor

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd7c45dfcf1bed13c0379eba51bd3ab44::getLoader();
+return ComposerAutoloaderInit4f498f2eef9b80206241e708b85ef590::getLoader();
