3a1ef1e47e0ec26f36325fd253cb51ea62f9b8d1
Selim Reza <selim@edutechsolutionsbd.com>
2015-03-10 10:34:43 +0600

    amw side bar updated

---
+++ app/views/layouts/_sidebar_amw.blade.php
@@ -0,0 +1,52 @@
+<ul class="sidebar-menu">
+    <li class="active">
+        <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+        </a>
+    </li>
+
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-bar-chart-o"></i>
+            <span>Common</span>
+            <i class="fa fa-angle-left pull-right"></i>
+        </a>
+        <ul class="treeview-menu">
+            <li><a href="#"><i class="fa fa-angle-double-right"></i> Common</a></li>
+        </ul>
+    </li>
+
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-bar-chart-o"></i>
+            <span>Academic</span>
+            <i class="fa fa-angle-left pull-right"></i>
+        </a>
+        <ul class="treeview-menu">
+            <li><a tabindex="-1" a href="{{URL::to('academic/amw/') }}">Mark Distribution (Amw)</a></li>
+        </ul>
+    </li>
+
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-bar-chart-o"></i>
+            <span>Admission</span>
+            <i class="fa fa-angle-left pull-right"></i>
+        </a>
+        <ul class="treeview-menu">
+            <li><a tabindex="-1" a href="{{URL::to('amw/course_manage') }}"> Course Management(Amw) </a></li>
+            <li><a tabindex="-1" a href="{{URL::to('amw/degree_manage') }}"> Degree Management(Amw) </a></li>
+        </ul>
+    </li>
+    <li class="treeview">
+            <a href="#">
+                <i class="fa fa-bar-chart-o"></i>
+                <span>Examination</span>
+                <i class="fa fa-angle-left pull-right"></i>
+            </a>
+            <ul class="treeview-menu">
+                <li><a href="#"><i class="fa fa-angle-double-right"></i> Common</a></li>
+            </ul>
+        </li>
+
+</ul>
