c42b82bed4f56e6607f550279e124c23826c403c
tanin <tanin@edutechsolutionsbd.com>
2015-03-11 14:21:40 +0600

    Issue # 52 app/modules/admission/views/adm_public/admission/degree_detail.blade.php changed

--- app/modules/admission/views/adm_public/admission/degree_detail.blade.php
+++ app/modules/admission/views/adm_public/admission/degree_detail.blade.php
@@ -4,96 +4,130 @@
 @stop
 @section('content')
 
-<h3>Degree Details</h3>
-<div class="well well-sm">
+<div class="box box-solid box-info">
+     <div class="box-header">
+             <h3 class="box-title">Admission On</h3>
+             <div class="box-tools pull-right">
+                 <button class="btn btn-info btn-sm" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                 <button class="btn btn-info btn-sm" data-widget="remove"><i class="fa fa-times"></i></button>
+             </div>
+     </div>
 
-   <table id="example1" class="table table-bordered table-striped">
+     <div class="box box-info">
+            <div class="box-header">
+            <p>&nbsp;</p>
+            </div>
+            <div class="box-body">
+                 <div class="row">
 
-      @foreach($degree_model as $values)
-            <tbody>
-                    <tr>
-                        <th width="20%">Degree Name</th>
-                        <td><b>{{$values->title}}</b></td>
-                    </tr>
-                     <tr>
-                        <th>Year</th>
-                        <td>{{$values->relYear->title}}</td>
-                    </tr>
-                    <tr>
-                        <th>Semester</th>
-                        <td>{{$values->relSemester->title}}</td>
-                    </tr>
-                    <tr>
-                        <th>Description</th>
-                        <td>{{$values->description}}</td>
-                    </tr>
+                     <div class="col-lg-12">
+                           <table class= "table table-bordered">
+                                  @foreach($degree_model as $values)
+                                         <tbody>
 
-                    <tr>
-                        <th>Total Credit:</th>
-                        <td>{{ $values->total_credit }}</td>
+                                         </tbody>
+                                  @endforeach
+                           </table>
 
-                    </tr>
+                     </div>
+                 </div>
+            </div>
 
-                    <tr>
-                        <th>Duration:</th>
-                        <td>{{ $values->duration }}</td>
-
-                    </tr>
-
-                    <tr>
-                        <th>Total Seat</th>
-                        <td>{{$values->seat}}</td>
-                    </tr>
-
-                     <tr>
-                        <th>Waiver (total: {{count($values->relDegreeWaiver)}} )</th>
-                        <td>
-                            @foreach($values->relDegreeWaiver as $waiver)
-                                {{$waiver->relWaiver->title}}<br>
-                            @endforeach
-                        </td>
-                    </tr>
-
-                     <tr>
-                        <th>Total Credit</th>
-                        <td>{{$values->total_credit}}</td>
-                     </tr>
-            </tbody>
-      @endforeach
-
-      @if(isset($major_courses))
-
-            <tbody>
-                <tr>
-                    <td>
-                        <div><b>Major Courses</b></div>
-                        @foreach($major_courses as $major_courses)
-                            {{$major_courses->relCourse->title}}<br>
-                        @endforeach
-                    </td>
-                    <td>
-                        <div><b>Minor Courses</b></div>
-                        @foreach($minor_courses as $minor_courses)
-                             {{$minor_courses->relCourse->title}}<br>
-                        @endforeach
-                    </td>
-                </tr>
-
-                <tr>
-                        <th>Minimum Requirements</th>
-
-                        <td>
-                           @foreach($edu_gpa_model as $value)
-                                Min GPA at {{$value->level_of_education.' : '.$value->min_gpa }}<br><br>
-                           @endforeach
-                        </td>
-                </tr>
-            </tbody>
-
-      @endif
-  </table>
-
+            <div class="box-footer clearfix">
+                <button class="pull-right btn btn-default" id="sendEmail">Edit <i class="fa fa-arrow-circle-right"></i></button>
+            </div>
+     </div>
+     
 </div>
+{{--<div class="well well-sm">--}}
+
+   {{--<table id="example1" class="table table-bordered table-striped">--}}
+
+      {{--@foreach($degree_model as $values)--}}
+            {{--<tbody>--}}
+                    {{--<tr>--}}
+                        {{--<th width="20%">Degree Name</th>--}}
+                        {{--<td><b>{{$values->title}}</b></td>--}}
+                    {{--</tr>--}}
+                     {{--<tr>--}}
+                        {{--<th>Year</th>--}}
+                        {{--<td>{{$values->relYear->title}}</td>--}}
+                    {{--</tr>--}}
+                    {{--<tr>--}}
+                        {{--<th>Semester</th>--}}
+                        {{--<td>{{$values->relSemester->title}}</td>--}}
+                    {{--</tr>--}}
+                    {{--<tr>--}}
+                        {{--<th>Description</th>--}}
+                        {{--<td>{{$values->description}}</td>--}}
+                    {{--</tr>--}}
+
+                    {{--<tr>--}}
+                        {{--<th>Total Credit:</th>--}}
+                        {{--<td>{{ $values->total_credit }}</td>--}}
+
+                    {{--</tr>--}}
+
+                    {{--<tr>--}}
+                        {{--<th>Duration:</th>--}}
+                        {{--<td>{{ $values->duration }}</td>--}}
+
+                    {{--</tr>--}}
+
+                    {{--<tr>--}}
+                        {{--<th>Total Seat</th>--}}
+                        {{--<td>{{$values->seat}}</td>--}}
+                    {{--</tr>--}}
+
+                     {{--<tr>--}}
+                        {{--<th>Waiver (total: {{count($values->relDegreeWaiver)}} )</th>--}}
+                        {{--<td>--}}
+                            {{--@foreach($values->relDegreeWaiver as $waiver)--}}
+                                {{--{{$waiver->relWaiver->title}}<br>--}}
+                            {{--@endforeach--}}
+                        {{--</td>--}}
+                    {{--</tr>--}}
+
+                     {{--<tr>--}}
+                        {{--<th>Total Credit</th>--}}
+                        {{--<td>{{$values->total_credit}}</td>--}}
+                     {{--</tr>--}}
+            {{--</tbody>--}}
+      {{--@endforeach--}}
+
+      {{--@if(isset($major_courses))--}}
+
+            {{--<tbody>--}}
+                {{--<tr>--}}
+                    {{--<td>--}}
+                        {{--<div><b>Major Courses</b></div>--}}
+                        {{--@foreach($major_courses as $major_courses)--}}
+                            {{--{{$major_courses->relCourse->title}}<br>--}}
+                        {{--@endforeach--}}
+                    {{--</td>--}}
+                    {{--<td>--}}
+                        {{--<div><b>Minor Courses</b></div>--}}
+                        {{--@foreach($minor_courses as $minor_courses)--}}
+                             {{--{{$minor_courses->relCourse->title}}<br>--}}
+                        {{--@endforeach--}}
+                    {{--</td>--}}
+                {{--</tr>--}}
+
+                {{--<tr>--}}
+                        {{--<th>Minimum Requirements</th>--}}
+
+                        {{--<td>--}}
+                           {{--@foreach($edu_gpa_model as $value)--}}
+                                {{--Min GPA at {{$value->level_of_education.' : '.$value->min_gpa }}<br><br>--}}
+                           {{--@endforeach--}}
+                        {{--</td>--}}
+                {{--</tr>--}}
+            {{--</tbody>--}}
+
+      {{--@endif--}}
+  {{--</table>--}}
+
+{{--</div>--}}
 
 <p>&nbsp;</p>
 <p>&nbsp;</p>
