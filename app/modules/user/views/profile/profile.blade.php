742c18f4e13664c1f49719da69a28490b407d7f0
Selim Reza <selim@edutechsolutionsbd.com>
2015-03-12 12:24:42 +0600

    profile

--- app/modules/user/views/profile/profile.blade.php
+++ app/modules/user/views/profile/profile.blade.php
@@ -9,6 +9,24 @@
    <section class="col-lg-6 connectedSortable">
 
 
+<div class="row">
+    <div class='col-sm-6'>
+        <div class="form-group">
+            <div class='input-group date' id='datetimepicker1'>
+                <input type='text' class="form-control" />
+                <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
+                </span>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+    $(function () {
+    $('#datetimepicker1').datetimepicker();
+    });
+    </script>
+</div>
+
+
        <!-- Profile Information -->
        <div class="box box-info">
            <div class="box-header">
