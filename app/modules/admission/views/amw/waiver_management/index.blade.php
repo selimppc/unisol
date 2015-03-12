d9c1d0b6d8dc20a357f93fbf3da9b2dfadac9d77
tanin <tanin@edutechsolutionsbd.com>
2015-03-12 11:10:51 +0600

    Issue #52 app/modules/admission/views/adm_public/admission/degree_list.blade.php changed

--- app/modules/admission/views/amw/waiver_management/index.blade.php
+++ app/modules/admission/views/amw/waiver_management/index.blade.php
@@ -73,7 +73,7 @@
 <p> &nbsp; </p>
 {{---------------------------------------------------Modals-----------------------------------------------}}
  <!-- Modal :: add Information -->
-<div data-backdrop="static" data-keyboard="false" class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div  class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
      <div class="modal-dialog">
          <div class="modal-content">
 
