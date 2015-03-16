08e5e0d141b0bffb7c4796e2c5cf47ef1d817c7a
Selim Reza <selim@edutechsolutionsbd.com>
2015-03-16 17:58:36 +0600

    models are related

--- app/modules/common/models/Waiver.php
+++ app/modules/common/models/Waiver.php
@@ -6,22 +6,63 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 
 class Waiver extends Eloquent{
 
+    //TODO :: model attributes and rules and validation
     protected $table='waiver';
-    protected $fillable= array('title', 'description', 'waiver_type', 'is_percentage', 'amount', 'acm_billing_item_id', 'created_by', 'updated_by');
+    protected $fillable = [
+        'title', 'description', 'waiver_type', 'is_percentage', 'amount', 'billing_details_id',
+    ];
+    private $errors;
+    private $rules = [
+        'title' => 'required|integer',
+        'description' => 'alpha',
+        'waiver_type' => 'alpha',
+        'is_percentage' => 'integer',
+        'amount' => 'numeric',
+        'billing_details_id' => 'required|integer',
+    ];
+    public function validate($data)
+    {
+        $validate = Validator::make($data, $this->rules);
+        if ($validate->fails())
+        {
+            $this->errors = $validate->errors();
+            return false;
+        }
+        return true;
+    }
+    public function errors()
+    {
+        return $this->errors;
+    }
 
-    public function relBillingDetails() {
+    //TODO : Model Relationship
+    public function relBatchWaiver(){
+        return $this->HasMany('BatchWaiver');
+    }
+    public function relBillingDetails(){
         return $this->belongsTo('BillingDetails', 'billing_details_id', 'id');
     }
 
+    // TODO : user info while saving data into table
     public static function boot(){
         parent::boot();
         static::creating(function($query){
-            $query->created_by = Auth::user()->get()->id;
-            $query->updated_by = Auth::user()->get()->id;
+            if(Auth::user()->check()){
+                $query->created_by = Auth::user()->get()->id;
+            }elseif(Auth::applicant()->check()){
+                $query->created_by = Auth::applicant()->get()->id;
+            }
         });
         static::updating(function($query){
-            $query->updated_by = Auth::user()->get()->id;
+            if(Auth::user()->check()){
+                $query->updated_by = Auth::user()->get()->id;
+            }elseif(Auth::applicant()->check()){
+                $query->updated_by = Auth::applicant()->get()->id;
+            }
         });
     }
+
+
+    //TODO : Scope Area
 
 } 
