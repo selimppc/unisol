203fd75a6a7e0ad325083f583dba3f38e3f3e004
ratnaakter <ratna@edutechsolutionsbd.com>
2015-03-17 13:08:56 +0600

    Issue #69 department update code

--- app/modules/common/views/department/edit.blade.php
+++ app/modules/common/views/department/edit.blade.php
@@ -1,30 +1,24 @@
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-    <h4 class="modal-title" id="myModalLabel">Edit</h4>
+    <h4 class="modal-title" style="text-align: center;color: #800080;font-size: x-large">Edit Department</h4>
 </div>
 <div class="modal-body">
-    <div style="padding: 20px;">
-        <h3>Edit {{$department->title}}</h3>
-        {{--@foreach($department as $data)--}}
+    <div style="padding: 10px; width: 90%;">
         {{Form::open(array('url'=>'department/update/'.$department->id, 'class'=>'form-horizontal'))}}
-        {{ Form::label('dept_name','Department Name:') }}
-        {{ Form::text('dept_name',$department->title, array('class' => 'form-control')) }}
-
-        <div class="form-group">
-            <div>{{ Form::label('dept_head_user_id', 'Department Head') }}</div>
-            <div>{{ Form::select('dept_head_user_id', $facultyList,($department->dept_head_user_id) ? $department->dept_head_user_id : Input::old('dept_head_user_id'),['class'=>'form-control','required']) }}</div>
-        </div>
-
-        {{ Form::label('description', 'Description:') }}
-        {{ Form::textarea('description',$department->description, array('class' => 'form-control')) }}
+        <div>{{ Form::label('dept_name','Department Name:') }}</div>
+        <div> {{ Form::text('dept_name',$department->title, array('class' => 'form-control')) }}</div>
+        <div>{{ Form::label('dept_head_user_id', 'Department Head') }}</div>
+        <div>{{ Form::select('dept_head_user_id', $facultyList,($department->dept_head_user_id) ? $department->dept_head_user_id : Input::old('dept_head_user_id'),['class'=>'form-control','required']) }}</div>
+        <div>{{ Form::label('description', 'Description:') }}</div>
+        <div>{{ Form::textarea('description',$department->description, array('class' => 'form-control')) }}</div>
         <p>&nbsp;</p>
-
         {{ Form::submit('Save Changes', array('class'=>'btn btn-primary')) }}
         <a href="" class="btn btn-default" span class="glyphicon-refresh">Close</a>
         {{Form::close()}}
     </div>
 </div>
-<br><br>
 <div class="modal-footer">
 </div>
+
+
 
