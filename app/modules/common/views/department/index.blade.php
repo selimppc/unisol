27877afb55e0dbe106d36d7945f2f3fc59c75bc6
ratnaakter <ratna@edutechsolutionsbd.com>
2015-03-17 12:42:58 +0600

    Issue #69 department update code

--- app/modules/common/views/department/index.blade.php
+++ app/modules/common/views/department/index.blade.php
@@ -108,15 +108,7 @@
     <div class="modal fade" id="myeditModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                    <h4 class="modal-title" id="myModalLabel">Edit</h4>
-                </div>
-                <div class="modal-body">
-                </div>
-                <br><br>
-                <div class="modal-footer">
-                </div>
+
             </div>
         </div>
     </div>
