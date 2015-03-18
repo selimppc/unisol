c4501a39c475872334d876744ef64af86cd97f19
ratnaakter <ratna@edutechsolutionsbd.com>
2015-03-18 15:37:30 +0600

    Issue #70 update code

--- app/modules/admission/views/amw/degree_courses/index.blade.php
+++ app/modules/admission/views/amw/degree_courses/index.blade.php
@@ -5,13 +5,11 @@
 @section('content')
     <div class="box box-solid ">
         <div class="box-header">
-            {{--<h4 class="box-title" style="color:#800080;text-align: center ">List Of all Course </h4>--}}
-
             {{ Form::open(array('url' => 'admission/amw/degree_courses/save')) }}
             {{ Form::hidden('degree_id', 1 , ['class'=>'form-control degree_id'])}}
             <div class="form-group" style="width: 350px">
-            {{ Form::label('course_list[]', 'List Of all Course ') }}
-            {{ Form::select('course_list[]', $course_list, Request::old('course_list') ? Request::old('course_list') : $course_list,['multiple' => true, 'class'=>'form-control']); }}
+                {{ Form::label('course_list[]', 'List Of all Course ') }}
+                {{ Form::select('course_list[]', $course_list, Request::old('course_list') ? Request::old('course_list') : $course_list,['multiple' => true, 'class'=>'form-control']); }}
             </div>
             {{ Form::submit('Add Course', ['class'=>'btn btn-primary']) }}
             {{ Form::close() }}
@@ -31,31 +29,27 @@
                         </tr>
                         </thead>
                         <tbody>
-                        {{--@foreach ($datas as $value)--}}
+                        @foreach ($deg_course_info as $value)
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td>{{$value->relCourse->title}}</td>
+                                <td>{{$value->relCourse->course_code}}</td>
+                                <td>{{$value->relCourse->relSubject->relDepartment->title}}</td>
+                                <td>{{$value->relCourse->relCourseType->title}}</td>
+                                <td>{{$value->relCourse->credit}}</td>
                                 <td>
-                                    <a href="" class="btn btn-xs btn-default" data-toggle="modal" data-target="#showOne" href=""><i class="fa fa-eye" style="color: green"></i></a>
+                                    <a data-href="{{ URL::to('year/delete/'.$value->id) }}" class="btn btn-xs btn-default" data-toggle="modal" data-target="#confirm-delete" href="" ><i class="fa  fa-trash-o" style="color: red"></i></a>
                                 </td>
                             </tr>
-                        {{--@endforeach--}}
+                        @endforeach
                         </tbody>
                     </table>
                     {{ Form::close() }}
-
-                    {{--{{ $datas->links() }}--}}
-
+                    {{ $deg_course->links() }}
                     <p>&nbsp;</p>
                     <p>&nbsp;</p>
-
                 </div>
             </div>
         </div>
     </div>
-
 
 @stop
