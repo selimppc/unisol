95d08499bf2910632afe3a61af38bbf3386b2c1c
Selim Reza <selim@edutechsolutionsbd.com>
2015-03-19 10:32:28 +0600

    migration updated for batch table

--- app/database/migrations/2015_03_15_110308_create_admission.php
+++ app/database/migrations/2015_03_15_110308_create_admission.php
@@ -53,7 +53,7 @@ class CreateAdmission extends Migration {
             $table->string('seat_total', 16);
             $table->dateTime('admission_deadline');
             $table->dateTime('admtest_date');
-            $table->dateTime('admtest_start_time');
+            $table->time('admtest_start_time');
             $table->tinyInteger('status', false)->lenght(1);
             $table->integer('created_by', false, 11);
             $table->integer('updated_by', false, 11);
