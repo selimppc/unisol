2d9c33ea622143e251eccd6d4e78e7b180577e48
ratnaakter <ratna@edutechsolutionsbd.com>
2015-04-05 10:05:54 +0600

    Issue #91 applicant miscellaneous info update code

--- app/views/layouts/_sidebar_applicant.blade.php
+++ app/views/layouts/_sidebar_applicant.blade.php
@@ -35,11 +35,11 @@
         </a>
         <ul class="treeview-menu">
             <a href="{{URL::to('applicant/profile/') }}"  class="list-group-item" >Profile</a>
-            <a href="{{URL::to('apt/acm_records/index') }}" class="list-group-item ">Academic Records</a>
-            <a href="{{URL::to('apt/personal_info/index') }}" class="list-group-item ">Personal Information</a>
-            <a href="{{URL::to('apt/supporting_docs/index') }}" class="list-group-item ">Supporting Documents</a>
-            <a href="{{URL::to('apt/extra_curricular/index') }}" class="list-group-item ">Extra-curricular Activities</a>
-            <a href="{{URL::to('apt/misc_info/index') }}" class="list-group-item ">Miscellaneous Information</a>
+            <a href="{{URL::to('apt/acm_records/') }}" class="list-group-item ">Academic Records</a>
+            <a href="{{URL::to('apt/personal_info/') }}" class="list-group-item ">Personal Information</a>
+            <a href="{{URL::to('apt/supporting_docs/') }}" class="list-group-item ">Supporting Documents</a>
+            <a href="{{URL::to('apt/extra_curricular/') }}" class="list-group-item ">Extra-curricular Activities</a>
+            <a href="{{URL::to('apt/misc_info/') }}" class="list-group-item ">Miscellaneous Information</a>
         </ul>
     </li>
 
