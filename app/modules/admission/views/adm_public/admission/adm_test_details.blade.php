08fa8437b4170a42a26f6c8d377daea9cda17370
Selim Reza <selim@edutechsolutionsbd.com>
2015-04-05 15:40:03 +0600

    updated sortable

--- app/modules/admission/views/adm_public/admission/adm_test_details.blade.php
+++ app/modules/admission/views/adm_public/admission/adm_test_details.blade.php
@@ -93,7 +93,7 @@
                           <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#exmCenterModal">
                                 Launch demo modal
                           </button>
-                             <a class="pull-right btn btn-sm btn-info" href="{{ URL::route('admission.public.exm-center' , ['batch_applicant_id'=>$batch_applicant_id]) }}" data-toggle="modal" data-target="#exmCenterModal" >Change Exam Center Sequence</a>
+                             <a class="pull-right btn btn-sm btn-info" data-toggle="modal" data-target="#exmCenterModal"  >Change Exam Center Sequence</a>
                           @else
                              <a class="pull-right btn btn-sm btn-info"  href="{{URL::route('admission.public.exm-center',['batch_applicant_id'=>$batch_applicant_id])}}" data-toggle="modal" data-target="#exmCenterModal"><b></b> Exam Center Choice </a>
                           @endif
@@ -146,61 +146,13 @@
             <div class="row">
                     <!-- Left col -->
                     <section class="col-lg-12 connectedSortable">
-                        <!-- Custom tabs (Charts with tabs)-->
-                        <div class="nav-tabs-custom">
-                            <!-- Tabs within a box -->
-                            <ul class="nav nav-tabs pull-right">
-                                <li class="active"><a href="#revenue-chart" data-toggle="tab">Area</a></li>
-                                <li><a href="#sales-chart" data-toggle="tab">Donut</a></li>
-                                <li class="pull-left header"><i class="fa fa-inbox"></i> Sales</li>
-                            </ul>
-                        </div><!-- /.nav-tabs-custom -->
-
-                        <!-- Chat box -->
-                        <div class="box box-success">
-                            <div class="box-header">
-                                <i class="fa fa-comments-o"></i>
-                                <h3 class="box-title">Chat</h3>
-                                <div class="box-tools pull-right" data-toggle="tooltip" title="Status">
-                                    <div class="btn-group" data-toggle="btn-toggle" >
-                                        <button type="button" class="btn btn-default btn-sm active"><i class="fa fa-square text-green"></i></button>
-                                        <button type="button" class="btn btn-default btn-sm"><i class="fa fa-square text-red"></i></button>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div><!-- /.box (chat box) -->
-
-                        <!-- TO DO List -->
-                        <div class="box box-primary">
-                            <div class="box-header">
-                                <i class="ion ion-clipboard"></i>
-                                <h3 class="box-title">To Do List</h3>
-                                <div class="box-tools pull-right">
-                                    <ul class="pagination pagination-sm inline">
-                                        <li><a href="#">&laquo;</a></li>
-                                        <li><a href="#">1</a></li>
-                                        <li><a href="#">2</a></li>
-                                        <li><a href="#">3</a></li>
-                                        <li><a href="#">&raquo;</a></li>
-                                    </ul>
-                                </div>
-                            </div><!-- /.box-header -->
-                        </div><!-- /.box -->
-
-                        <!-- quick email widget -->
-                        <div class="box box-info">
-                            <div class="box-header">
-                                <i class="fa fa-envelope"></i>
-                                <h3 class="box-title">Quick Email</h3>
-                                <!-- tools box -->
-                                <div class="pull-right box-tools">
-                                    <button class="btn btn-info btn-sm" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
-                                </div><!-- /. tools -->
-                            </div>
-
-                        </div>
-
+                        @foreach(($exm_center_choice_lists) as $value)
+                            <div class="nav-tabs-custom" style="background: powderblue">
+                                <ul class="nav nav-tabs pull-right">
+                                    <li class="pull-left header"><i class="fa fa-inbox"></i> {{ $value->relExmCenter->title }}</li>
+                                </ul>
+                            </div><!-- /.nav-tabs-custom -->
+                         @endforeach
                     </section><!-- /.Left col -->
 
                 </div><!-- /.row (main row) -->
