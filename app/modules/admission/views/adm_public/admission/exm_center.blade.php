8195deb1c9f607d34620dbd8232c21215ac50835
tanin <tanin@edutechsolutionsbd.com>
2015-04-05 10:56:44 +0600

    Issue # 54 adm exam center choice

--- app/modules/admission/views/adm_public/admission/exm_center.blade.php
+++ app/modules/admission/views/adm_public/admission/exm_center.blade.php
@@ -1,11 +1,27 @@
+
 <div class="modal-header">
 
     <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
     <h4 class="modal-title" id="myModalLabel">Add Or Update</h4>
 </div>
 
+ <style>
+	ul { list-style-type: none; margin: 0; padding: 0; margin-bottom: 15px; }
+	li { margin: 10px; padding: 5px; width: 270px; }
+ </style>
+
+
 <div class="modal-body">
      <div style="padding: 20px;">
+
+     <ul id="sortable">
+     	<li class="ui-state-default">1. Dhaka</li>
+     	<li class="ui-state-default">2. Chittagong</li>
+     	<li class="ui-state-default">3. Comilla</li>
+     	<li class="ui-state-default">4. Barisal</li>
+     	<li class="ui-state-default">5. Syllhet</li>
+     </ul>
+
        {{ Form::open(['route' => ['admission.public.save-exm-center'], 'class'=>'form-horizontal','files' => true,]) }}
             {{ Form::hidden('batch_applicant_id', $ba_id )}}
             @if(isset($exm_centers_all))
@@ -30,3 +46,23 @@
        {{Form::close()}}
      </div>
 </div>
+
+
+<script>
+	$(function() {
+		$( "#sortable" ).sortable({
+			revert: true
+		});
+		$( "#draggable" ).draggable({
+			connectToSortable: "#sortable",
+			helper: "clone",
+			revert: "invalid"
+		});
+		$( "ul, li" ).disableSelection();
+	});
+</script>
+
+{{ HTML::style('assets/css/jquery-ui.css') }}
+{{ HTML::script('assets/js/jquery-1.10.2.js')}}
+
+
