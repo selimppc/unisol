cd33d9b15d98323ef28e2ad7cb88f9e956e176d3
Selim Reza <selim@edutechsolutionsbd.com>
2015-04-08 10:31:49 +0600

    applicant admission

--- app/modules/applicant/controllers/ApplicantAdmissionController.php
+++ app/modules/applicant/controllers/ApplicantAdmissionController.php
@@ -18,4 +18,27 @@ class ApplicantAdmissionController extends \BaseController {
 
     }
 
+
+    public function admission_test_subject($batch_id)
+    {
+        $test_subject = BatchAdmtestSubject::with('relBatch','relAdmtestSubject','relAdmQuestion')
+            ->where('batch_id', '=', $batch_id)
+            ->get();
+
+        $batch_applicant = BatchApplicant::with('relBatch','relBatch.relDegree','relBatch.relSemester','relBatch.relYear')
+            ->where('batch_id', '=', $batch_id)
+            ->get();
+
+        return View::make('applicant::admission_test.subject_details',compact('test_subject','batch_applicant','question'));
+    }
+
+    public function admission_test_subject_exam($batch_admtest_sub_id)
+    {
+        $question = AdmQuestion::with('relBatchAdmtestSubject','relBatchAdmtestSubject.relAdmtestSubject','relAdmQuestionItems','relAdmQuestionItems.relAdmQuestionOptAns')
+            ->where('batch_admtest_subject_id', '=', $batch_admtest_sub_id)
+            ->get();
+
+        return View::make('applicant::admission_test.subject_exam',compact('question'));
+    }
+
 }
