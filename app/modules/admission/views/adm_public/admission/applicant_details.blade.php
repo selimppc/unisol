2ca9c747278b1faee559772432bd18db4fe745e2
tanin <tanin@edutechsolutionsbd.com>
2015-04-09 10:11:34 +0600

    Issue # 54 admpublic

--- app/modules/admission/views/adm_public/admission/applicant_details.blade.php
+++ app/modules/admission/views/adm_public/admission/applicant_details.blade.php
@@ -82,7 +82,8 @@
                   <div class="row">
                       <div class="col-lg-4">
                               @if(isset($applicant_personal_info))
-                                  {{ HTML::image('applicant_images/profile/'.$applicant_personal_info->profile_image) }}
+                                  {{--{{ HTML::image('applicant_images/profile/'.$applicant_personal_info->profile_image) }}--}}
+                                  <td class="etsb-image-doc">{{ $applicant_personal_info->profile_image != null ? HTML::image('applicant_images/profile/'.$applicant_personal_info->profile_image) :  HTML::image('/img/profile.jpg') }}</td>
                               @endif
                       </div>
                       <div class="col-lg-8">
