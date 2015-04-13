e8c82e76ff2eb8c1ea5ee774090af0abc780109d
shafiqul haque <shafi@edutechsolutionsbd.com>
2015-04-13 10:08:09 +0600

    updates

--- app/modules/admission/controllers/AdmFacultyController.php
+++ app/modules/admission/controllers/AdmFacultyController.php
@@ -67,10 +67,10 @@ class AdmFacultyController extends \BaseController {
 
 
         $search_index_adm_examiner = AdmExaminer::join('Batch', function ($query) use ($year_id, $semester_id) {
-            $query->on('batch.semester_id', '=', 'adm_examiner.batch_id');
+            $query->on('batch.id', '=', 'adm_examiner.batch_id');
 
-            if (isset($year_id) && !empty($year_id)) $query->where('adm_examiner.batch_id', '=', $year_id);
-            if (isset($semester_id) && !empty($semester_id)) $query->where('adm_examiner.batch_id', '=', $semester_id);
+            if (isset($year_id) && !empty($year_id)) $query->where('batch.year_id', '=', $year_id);
+            if (isset($semester_id) && !empty($semester_id)) $query->where('batch.semester_id', '=', $semester_id);
         });
 
         $search_index_adm_examiner = $search_index_adm_examiner->select(['batch.semester_id as sem_id', 'batch.year_id as yr_id']);
