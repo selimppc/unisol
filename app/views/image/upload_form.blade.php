231ffe6f793c5c5fd4837e31e3b1a2eab444716b
Selim Reza <selim@edutechsolutionsbd.com>
2015-04-16 11:15:56 +0600

    uploads

--- app/views/image/upload_form.blade.php
+++ app/views/image/upload_form.blade.php
@@ -1,13 +1,14 @@
 
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>
-    <script src="http://malsup.github.com/jquery.form.js"></script>
+    {{ HTML::script('assets/etsb/etsb_js/jquery/jquery-2.1.1.min.js')}}
+
+    {{ HTML::script('assets/etsb/etsb_js/jquery/jquery.form.js')}}
 
     <div class="span8">
         <!-- Post Title -->
         <div class="row">
             <div class="span8">
-                <h4>Ajax Image Upload and Preview With Laravel</h4>
+                <h4>Ajax Image Upload and Preview </h4>
             </div>
         </div>
         <!-- Post Footer -->
@@ -15,10 +16,8 @@
             <div class="span3">
                 <div id="validation-errors"></div>
                 <form class="form-horizontal" id="upload" enctype="multipart/form-data" method="post" action="{{ url('upload/image') }}" autocomplete="off">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
                     <input type="file" name="image" id="image" /> 
                 </form>
- 
             </div>
             <div class="span5">
                 <div id="output" style="display:none">
@@ -27,38 +26,4 @@
         </div>
     </div>
 
-
-<script>
-    $(document).ready(function() {
-        var options = {
-                    beforeSubmit:  showRequest,
-            success:       showResponse,
-            dataType: 'json'
-            };
-        $('body').delegate('#image','change', function(){
-            $('#upload').ajaxForm(options).submit();
-        });
-    });
-    function showRequest(formData, jqForm, options) {
-        $("#validation-errors").hide().empty();
-        $("#output").css('display','none');
-        return true;
-    }
-    function showResponse(response, statusText, xhr, $form)  {
-        if(response.success == false)
-        {
-            var arr = response.errors;
-            $.each(arr, function(index, value)
-            {
-                if (value.length != 0)
-                {
-                    $("#validation-errors").append('<div class="alert alert-error"><strong>'+ value +'</strong><div>');
-                }
-            });
-            $("#validation-errors").show();
-        } else {
-             $("#output").html("<img src='"+response.file+"' />");
-             $("#output").css('display','block');
-        }
-    }
-</script>
+@include('image.script_image')
