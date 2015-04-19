4f46a64fb8b1c9491d55c6de8ee0fd2ebff0844e
shafiqul haque <shafi@edutechsolutionsbd.com>
2015-04-19 16:33:19 +0600

    Updates Bug issue 34

--- app/modules/admission/views/amw/adm_question/re_assign_faculty_by_question_comnnets.blade.php
+++ app/modules/admission/views/amw/adm_question/re_assign_faculty_by_question_comnnets.blade.php
@@ -1,20 +1,20 @@
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-    <h4>Assign Examiner and Comments </h4>
+    <h4>Re-Assign Examiner and Comments </h4>
 </div>
 
 <div style="padding: 4%; ">
-    {{ Form::open(array('route'=>['admission.amw.admission-test.comments-by-question',$question_data->id],'method' => 'POST')) }}
-            {{ Form::hidden('adm_question_id', $q_id ) }}
-            {{ Form::hidden('commented_to', $question_data->s_faculty_user_id) }}
+    {{ Form::open(array('route'=>['admission.amw.admission-test.comments-re-assign',$question_data->id],'method' => 'POST')) }}
+        {{Form::hidden('adm_question_id', $q_id )}}
+        {{Form::hidden('commented_to', $question_data->s_faculty_user_id)}}
 
             <h5> <strong>Batch # </strong>{{$question_data->relBatchAdmtestSubject->relBatch->batch_number}} </h5>
             <h5>  <strong>Degree Name : </strong>
-                {{$question_data->relBatchAdmtestSubject->relBatch->relDegree->relDegreeLevel->code}}
-                {{$question_data->relBatchAdmtestSubject->relBatch->relDegree->relDegreeGroup->code}} In
-                 {{$question_data->relBatchAdmtestSubject->relBatch->relDegree->relDegreeProgram->code}},
-                {{$question_data->relBatchAdmtestSubject->relBatch->relSemester->title}} -
-                {{$question_data->relBatchAdmtestSubject->relBatch->relYear->title}}
+                {{ $question_data->relBatchAdmtestSubject->relBatch->relDegree->relDegreeLevel->code }}
+                {{ $question_data->relBatchAdmtestSubject->relBatch->relDegree->relDegreeGroup->code }} In
+                {{ $question_data->relBatchAdmtestSubject->relBatch->relDegree->relDegreeProgram->code }},
+                {{ $question_data->relBatchAdmtestSubject->relBatch->relSemester->title }} -
+                {{ $question_data->relBatchAdmtestSubject->relBatch->relYear->title }}
             </h5>
 
             <strong>Subject : </strong>{{ $question_data->relBatchAdmtestSubject->relAdmtestSubject->title }}
@@ -32,6 +32,7 @@
                   &nbsp; &nbsp; &nbsp; {{ $comment->comment }}
                 </p>
             @endforeach
+
 
             <div class="form-group">
                   {{ Form::textarea('comment', Null, ['class' => 'form-control', 'placeholder'=>'Your Comments Here', 'style'=>'height: 100px;']) }}
