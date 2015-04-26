c1598099ea21b827f7c975940d48c7b3fc3e00aa
shafiqul haque <shafi@edutechsolutionsbd.com>
2015-04-26 11:34:55 +0600

    updates

--- app/modules/admission/views/amw/degree/degree/index.blade.php
+++ app/modules/admission/views/amw/degree/degree/index.blade.php
@@ -9,79 +9,78 @@
     <div class="col-md-12">
                 <h3>Degree</h3>
             <div class="help-text-top">
-                    You can view all lists of Degree Lists. Also this panel will allow you to perform
-                    some actions to <b>Add Degree</b>, <b>Edit</b>, <b>Delete</b>, <b>Degree Course (DC)</b>
-                    under the column <b>Action</b>. Button <b>Degree Course (DC)</b> will redirect you to
-                    degree course manage screen.
+             You can view all lists of Degree Lists. Also this panel will allow you to perform some actions to <b>Add Degree</b>, <b>Edit</b>, <b>Delete</b>, <b>Degree Course (DC)</b> under the column <b>Action</b>. Button <b>Degree Course (DC)</b> will redirect you to degree course manage screen.
                     {{--<small>Someone famous in <cite title="Source Title">Source Title</cite></small>--}}
             </div><!-- /.box-body -->
     </div><!-- ./col -->
 </div><!-- /.row -->
 {{-----------------------------------------Help Text ends ----------------------------------------------------------------------}}
 
+    <div class="box box-solid ">
+        <div class="box box-info">
 
-            {{-------------------Searching Starts--------------------------------------------------------------}}
+{{-------------------Searching Starts--------------------------------------------------------------}}
 
-                <p>{{ Form::open(array('url'=>'admission/amw/degree/search','class'=>'form-horizontal')) }}
-                        <div  class="col-lg-3">
-                            <div class="input-group input-group-sm">
-                                {{ Form::select('search_department', $department , Input::old('search_department'),['class'=>'form-control input-sm '])}}
-                                <span class="input-group-btn">
-                                    <button class="btn btn-info btn-flat" type="submit" title="Search ..">Search</button>
-                                </span>
-                            </div>
-                        </div>
-                        <div class="col-lg-9" class="box-tools pull-right">
-                            <a class="pull-right btn btn-sm btn-info" href="{{ URL::to('admission/amw/degree/create')}}" data-toggle="modal" data-target="#degreeModal" style="color: #ffffff" title="New Degree"><b>Add Degree</b></a>
+            <p>{{ Form::open(array('url'=>'admission/amw/degree/search','class'=>'form-horizontal')) }}
+                <div  class="col-lg-3">
+                    <div class="input-group input-group-sm">
+                        {{ Form::select('search_department', $department , Input::old('search_department'),['class'=>'form-control input-sm '])}}
+                        <span class="input-group-btn">
+                            <button class="btn btn-info btn-flat" type="submit" title="Search ..">Search</button>
+                        </span>
+                    </div>
+                </div>
+                <div class="col-lg-9" class="box-tools pull-right">
+                    <a class="pull-right btn btn-sm btn-info" href="{{ URL::to('admission/amw/degree/create')}}" data-toggle="modal" data-target="#degreeModal" style="color: #ffffff" title="New Degree"><b>Add Degree</b></a>
 
-                        </div>
+                </div>
 
-                    {{ Form::close() }}
-                </p>
-            {{------------Searching Ends--------------------------------------------------------------------}}
-
-            <div class="box box-solid ">
+            {{ Form::close() }}
+            </p>
+{{------------Searching Ends--------------------------------------------------------------------}}
+            </div>
+            <div class="box-body">
                 <div class="row">
                     <div class="col-lg-12">
-                        <br>
+                    <br>
                         {{ Form::open(array('route' => 'admission.amw.degree.dg_batch_delete')) }}
                         @if($model->count())
-                        <div class="box-body">
-                            <table id="example" class="table table-striped table-bordered">
-                                <thead>
+                        <table id="example" class="table table-bordered">
+                            <thead>
+                            <tr>
+                                <th>
+                                    <input name="id" type="checkbox" id="checkbox" class="checkbox" value="">
+                                </th>
+                                <th>Degree(Title)</th>
+                                <th>Department </th>
+                                <th>Total Credit</th>
+                                <th>Duration</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @if(isset($model))
+                                @foreach($model as $value)
                                     <tr>
-                                        <th><input name="id" type="checkbox" id="checkbox" class="checkbox" value=""></th>
-                                        <th>Degree(Title)</th>
-                                        <th>Department </th>
-                                        <th>Total Credit</th>
-                                        <th>Duration</th>
-                                        <th>Action</th>
+                                        <td><input type="checkbox" name="id[]"  class="myCheckbox" value="{{ $value->id }}">
+                                        </td>
+                                        <td>{{ $value->relDegreeLevel->code.''.$value->relDegreeGroup->code.' In '.$value->relDegreeProgram->code }}</td>
+                                        <td>{{ $value->relDepartment->title }}</td>
+                                        <td>{{ $value->total_credit}}</td>
+                                        <td>{{ $value->duration}}</td>
+                                        <td>
+                                            <a href="{{ URL::route('admission.amw.degree.show', ['id'=>$value->id]) }}" class="btn btn-xs btn-default" data-toggle="modal" data-target="#degreeModal" style="font-size: 12px;color: darkmagenta" title="View"><span class="fa fa-eye"></span></a>
+                                            <a class="btn btn-xs btn-default" href="{{ URL::route('admission.amw.degree.edit', ['id'=>$value->id]) }}" data-toggle="modal" data-target="#degreeModal" style="font-size: 12px;color: lightseagreen"><i class="fa fa-edit" title="Update"></i></a>
+                                            <a data-href="{{ URL::to('admission/amw/degree/delete/'.$value->id) }}" class="btn btn-xs btn-default" data-toggle="modal" data-target="#confirm-delete" style="font-size: 12px;color: lightcoral" title="Remove"><span class="fa  fa-trash-o"></span></a>
+                                            <a href="{{ URL::route('admission.amw.degree_courses', ['id'=>$value->id])  }}" class="btn btn-xs btn-info" title="Degree Course"> DC </a>
+                                            <a href="{{ URL::route('admission.amw.batch', ['degree_id'=>$value->id])  }}" class="btn btn-xs btn-success" title="Batch Manage"> BM </a>
+                                        </td>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    @if(isset($model))
-                                        @foreach($model as $value)
-                                            <tr>
-                                                <td><input type="checkbox" name="id[]"  class="myCheckbox" value="{{ $value->id }}">
-                                                </td>
-                                                <td>{{ $value->relDegreeLevel->code.''.$value->relDegreeGroup->code.' In '.$value->relDegreeProgram->code }}</td>
-                                                <td>{{ $value->relDepartment->title }}</td>
-                                                <td>{{ $value->total_credit}}</td>
-                                                <td>{{ $value->duration}}</td>
-                                                <td>
-                                                    <a href="{{ URL::route('admission.amw.degree.show', ['id'=>$value->id]) }}" class="btn btn-xs btn-default" data-toggle="modal" data-target="#degreeModal" style="font-size: 12px;color: darkmagenta" title="View"><span class="fa fa-eye"></span></a>
-                                                    <a class="btn btn-xs btn-default" href="{{ URL::route('admission.amw.degree.edit', ['id'=>$value->id]) }}" data-toggle="modal" data-target="#degreeModal" style="font-size: 12px;color: lightseagreen"><i class="fa fa-edit" title="Update"></i></a>
-                                                    <a data-href="{{ URL::to('admission/amw/degree/delete/'.$value->id) }}" class="btn btn-xs btn-default" data-toggle="modal" data-target="#confirm-delete" style="font-size: 12px;color: lightcoral" title="Remove"><span class="fa  fa-trash-o"></span></a>
-                                                    <a href="{{ URL::route('admission.amw.degree_courses', ['id'=>$value->id])  }}" class="btn btn-xs btn-info" title="Degree Course"> DC </a>
-                                                    <a href="{{ URL::route('admission.amw.batch', ['degree_id'=>$value->id])  }}" class="btn btn-xs btn-success" title="Batch Manage"> BM </a>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    @endif
-                                </tbody>
-                                {{ Form::submit('Delete Items', array('class'=>'btn btn-xs btn-danger', 'id'=>'hide-button', 'style'=>'display:none'))}}
-                            </table>
-                        </div>
+                                @endforeach
+                            @endif
+                            </tbody>
+                            {{ Form::submit('Delete Items', array('class'=>'btn btn-xs btn-danger', 'id'=>'hide-button', 'style'=>'display:none'))}}
+                        </table>
                         @else
                             <p>&nbsp;</p>
                                 <div>
@@ -93,11 +92,11 @@
                     </div>
                 </div>
             </div>
-        {{--</div>--}}
-    {{--</div>--}}
-    {{--<div class="text-right">--}}
-{{--        {{ $model->links() }}--}}
-    {{--</div>--}}
+        </div>
+    </div>
+    <div class="text-right">
+        {{ $model->links() }}
+    </div>
 
 {{----------------------Modal : degreeGroupModal--------------------------------------------------------------------------}}
     <div class="modal fade" id="degreeModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
