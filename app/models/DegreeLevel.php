395a2fa67159f2dc5276c7d8efb4291564924412
Selim Reza <selim@edutechsolutionsbd.com>
2015-05-06 15:14:03 +0600

    degree level model

---
+++ app/models/DegreeLevel.php
@@ -0,0 +1,83 @@
+<?php
+
+
+class DegreeLevel extends Eloquent
+{
+
+    protected $table = 'degree_level';
+
+    private $errors;
+    // 1
+    private $rules = array(
+        'code' => 'required|unique:degree_level',
+        //'title' => 'required|unique:degree_level',
+        //'description'  => 'required',
+
+    );
+
+
+    public function validate($data)
+    {
+        // make a new validator object
+        $validate = Validator::make($data, $this->rules);
+        // check for failure
+        if ($validate->fails())
+        {
+            // set errors and return false
+            $this->errors = $validate->errors();
+            return false;
+        }
+        // validation pass
+        return true;
+    }
+
+    // 2
+
+    private $rules2 = array(
+        'title' => 'required',
+        'description'  => 'required',
+
+    );
+
+    public function validate2($data)
+    {
+        // make a new validator object
+        $validate = Validator::make($data, $this->rules2);
+        // check for failure
+        if ($validate->fails())
+        {
+            // set errors and return false
+            $this->errors = $validate->errors();
+            return false;
+        }
+        // validation pass
+        return true;
+    }
+
+
+
+
+
+    public function errors()
+    {
+        return $this->errors;
+    }
+
+    public static function getDegreeLevelName($degId){
+        $data = DegreeLevel::find($degId);
+        return $data->title;
+    }
+
+    public static function boot(){
+        parent::boot();
+        static::creating(function($query){
+            $query->created_by = Auth::user()->get()->id;
+        });
+        static::updating(function($query){
+            $query->updated_by = Auth::user()->get()->id;
+        });
+    }
+
+
+
+}
