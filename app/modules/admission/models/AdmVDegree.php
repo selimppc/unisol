1f81e1e7b62d2cd09ba7394f7bb680e852e61559
Almas Hossain <almas@edutechsolutionsbd.com>
2015-05-10 14:52:15 +0600

    Refactor: adm->amw->batch_course_index.

---
+++ app/modules/admission/models/AdmVDegree.php
@@ -0,0 +1,11 @@
+<?php
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class AdmVDegree extends Eloquent{
+
+    protected $table = 'adm_v_degree';
+
+} 
