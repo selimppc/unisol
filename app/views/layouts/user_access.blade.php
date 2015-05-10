1b8dc8d1ce6cff387f4833a780f801118061f8ac
Selim Reza <selim@edutechsolutionsbd.com>
2015-05-10 16:16:43 +0600

    update

--- app/views/layouts/user_access.blade.php
+++ app/views/layouts/user_access.blade.php
@@ -8,6 +8,30 @@
         });
     </script>--}}
 
+             <div class="modal-dialog">
+                 <div class="modal-content">
+                     <div class="modal-header">
+                         <span class="close_button" style="float: right">
+                             <a href="{{URL::previous()}}" class="btn btn-info "> <span aria-hidden="true">&times;</span> </a>
+                         </span>
+                         <h4 class="modal-title" id="myModalLabel">
+                             You are: <span style="font-weight: bold; color: #002a80;">
+                               {{isset(Auth::user()->get()->id) ? Auth::user()->get()->username : Auth::applicant()->get()->username}}
+                               </span>
+                         </h4>
+                     </div>
+                     <div class="modal-body">
+                          <h3 style="color: #ad6704;">You are not authorized to perform this action! </h3>
+                          <div>
+                             {{HTML::image('img/forbidden/forbidden_anime.png')}}
+                          </div>
+                     </div>
+                     <div class="modal-footer">
+                         <a href="{{URL::previous()}}" class="btn btn-primary "> < Go Back </a>
+                     </div>
+                 </div><!-- /.modal-content -->
+             </div><!-- /.modal-dialog -->
+
 
      <!-- User - Access Modal -->
      <div data-backdrop="static" data-keyboard="false" class="modal fade" id="user-access" tabindex="-1" role="dialog" aria-labelledby="addCategory" aria-hidden="false">
