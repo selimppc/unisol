a51e754af7e4dc0893bc09944a5c781717039912
Selim Reza <selim@edutechsolutionsbd.com>
2015-05-10 15:43:08 +0600

    Issues #102 :

--- app/start/global.php
+++ app/start/global.php
@@ -142,5 +142,22 @@ require_once app_path('logger.php');
 //|
 //*/
 
+
+/*
+|--------------------------------------------------------------------------
+| Application Error Handler
+|--------------------------------------------------------------------------
+|
+| Here you may handle any errors that occur in your application, including
+| logging them or displaying custom views for specific errors. You may
+| even register several error handlers to handle different types of
+| exceptions. If nothing is returned, the default error view is
+| shown, which includes a detailed stack trace during debug.
+|
+*/
+
+
+
+
 require app_path().'/filters.php';
 require_once app_path().'/helpers/helpers.php';
