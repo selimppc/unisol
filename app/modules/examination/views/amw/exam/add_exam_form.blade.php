cb137c4311adf94859e1ab796c66f0808e09fffe
tanin <tanin@edutechsolutionsbd.com>
2015-05-20 12:25:29 +0600

    Issue # 105 add exam item with answer

--- app/modules/examination/views/amw/exam/add_exam_form.blade.php
+++ app/modules/examination/views/amw/exam/add_exam_form.blade.php
@@ -11,38 +11,41 @@
           <div class="row">
               <div class="help-text-top">
                 <em>If you want to add a new examination, You have to fillup this form.  <span class="text-danger">  (*) </span>Indicates required field. Please do not skip these fields.</em>
+
               </div>
           </div>
 
           <div class="form-group">
-              {{ Form::label('title', 'Title') }}
-              {{ Form::text('title', Input::old('title'), array('class' => 'form-control')) }}
+              {{ Form::label('title', 'Title') }}<span class="text-danger">*</span>
+              {{ Form::text('title', Input::old('title'), array('class' => 'form-control','required'=>'required')) }}
           </div>
 
           <div class="form-group">
-              {{ Form::label('acm_marks_dist_item_id', 'Exam Type') }}
+              {{ Form::label('acm_marks_dist_item_id', 'Exam Type') }}<span class="text-danger">*</span>
               {{ Form::select('acm_marks_dist_item_id', $exam_type, Input::old('acm_marks_dist_item_id'), array('class' => 'form-control','required'=>'required')) }}
           </div>
 
           <div class="form-group">
-                {{ Form::label('semester_id', 'Semester') }}
-                {{ Form::select('semester_id', $semester_id, Input::old('semester_id'), array('class' => 'form-control', 'id'=>'sem-data')) }}
+                {{ Form::label('semester_id', 'Semester') }}<span class="text-danger">*</span>
+                {{ Form::select('semester_id', $semester_id, Input::old('semester_id'), ['class' => 'form-control', 'id'=>'sem-data','required'=>'required']) }}
           </div>
 
           <div class="form-group">
-              {{ Form::label('year_id', 'Year') }}
-              {{ Form::select('year_id',$year_id, Input::old('year_id'), ['id'=>'course_name','class'=>'form-control'] ) }}
+              {{ Form::label('year_id', 'Year') }}<span class="text-danger">*</span>  <em style="margin-left: 20px;color:#808080">(To Select Year,Please Select Semester At Before)</em>
+              {{ Form::select('year_id',$year_id, Input::old('year_id'), ['id'=>'course_name','class'=>'form-control','required'=>'required', 'onclick'=>'SelectSemester()'] ) }}
+          </div>
+          <div style="color:firebrick" id ="errors">
           </div>
 
            @if($course_list)
               <div class="form-group">
-                  {{ Form::label('course_conduct_id', 'Course Name') }}
+                  {{ Form::label('course_conduct_id', 'Course Name') }}<span class="text-danger">*</span>
                   {{ Form::select('course_conduct_id',$course_list, Input::old('course_conduct_id'),['id'=>'dependable-list', 'class'=>'form-control','placeholder'=>'']) }}
               </div>
           @else
               <div class="form-group">
-                 {{ Form::label('course_conduct_id', 'Course Name') }}
-                 {{ Form::select('course_conduct_id', $course_list, Input::old('course_conduct_id'), ['id'=>'dependable-list', 'class'=>'form-control','placeholder'=>'']) }}
+                 {{ Form::label('course_conduct_id', 'Course Name') }}<span class="text-danger">*</span>
+                 {{ Form::select('course_conduct_id', $course_list, Input::old('course_conduct_id'), ['id'=>'dependable-list', 'class'=>'form-control','placeholder'=>'','required'=>'required']) }}
               </div>
           @endif
 
@@ -67,4 +70,18 @@
             });
         });
  });
+ </script>
+
+{{------------------Error Message Show------------------}}
+ <script>
+  function SelectSemester(){
+
+     var semester_value =  document.getElementById("sem-data").value ;
+     if(semester_value === ""){
+       document.getElementById('errors').innerHTML="Please Select Semester At First.";
+     }else{
+     $("#errors").hide();
+     }
+    }
+
  </script>
