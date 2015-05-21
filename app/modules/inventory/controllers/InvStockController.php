1ae530accac60ebbf392a23884b5ad0ae361a797
Selim Reza <selim@edutechsolutionsbd.com>
2015-05-21 16:58:05 +0600

    view table adm_v_question_evaluation

---
+++ app/modules/inventory/controllers/InvStockController.php
@@ -0,0 +1,19 @@
+<?php
+
+class InvStockController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function stock_view()
+	{
+        $stock_view = "";
+
+	}
+
+
+
+
+}
