d8bb2b2858b26ca5f88b5cf9853024f1ca5e4f78
Selim Reza <selim@edutechsolutionsbd.com>
2015-05-21 09:48:29 +0600

    Issue #106 GRN

--- app/modules/inventory/controllers/InvGrnController.php
+++ app/modules/inventory/controllers/InvGrnController.php
@@ -22,9 +22,22 @@ class InvGrnController extends \BaseController {
      */
     public function index_grn()
     {
-        $pageTitle = 'Product Category';
-        $data = InvProductCategory::all();
-        return View::make('inventory::product_category.index', compact('pageTitle', 'data'));
+        $pageTitle = 'GRN History';
+        $data = InvGrnHead::all();
+        return View::make('inventory::grn.index', compact('pageTitle', 'data'));
+    }
+
+    public function create_grn($grn_id, $po_id){
+        $po_dt = InvPurchaseOrderDetail::where('inv_po_head_id', $po_id)->get();
+        $grn_dt = InvGrnDetail::where('inv_grn_head_id', $grn_id)->get();
+        return View::make('inventory::grn.detail', compact(
+            'po_dt' , 'po_id', 'grn_dt'
+        ));
+    }
+
+
+    public function show_grn_detail($grn_id){
+
     }
 
 }
