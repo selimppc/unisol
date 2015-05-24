bcc5725fff6cb7bbfdaf98b3ed1e09addee4c23c
shafiqul haque <shafi@edutechsolutionsbd.com>
2015-05-24 13:00:26 +0600

    migration updates

--- app/database/migrations/2015_05_11_121630_create_examination.php
+++ app/database/migrations/2015_05_11_121630_create_examination.php
@@ -132,7 +132,6 @@ class CreateExamination extends Migration {
             $table->increments('id');
             $table->unsignedInteger('exm_question_evaluation_id')->nullable();
             $table->text('answer');
-            $table->string('marks', 8);
             $table->integer('created_by', false, 11);
             $table->integer('updated_by', false, 11);
             $table->timestamps();
