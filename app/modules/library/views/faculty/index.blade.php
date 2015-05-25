6f3f4a5401653bd20fc1a58743058468836ddc79
tanin <tanin@edutechsolutionsbd.com>
2015-05-25 17:58:37 +0600

    Issue # 110 LibFacultyController modified

--- app/modules/library/views/faculty/index.blade.php
+++ app/modules/library/views/faculty/index.blade.php
@@ -17,20 +17,7 @@
            {{-------------- Filter :Starts -------------------------------------------}}
            <div>
                {{ Form::open(array('url' => 'library/faculty/book')) }}
-               {{--<div class="input-group input-group-sm">--}}
-                   {{--<div class="input-group-btn">--}}
-                       {{--<button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown">Action <span class="fa fa-caret-down"></span></button>--}}
-                       {{--<ul class="dropdown-menu">--}}
-                           {{--<li><a href="#">Action</a></li>--}}
-                           {{--<li><a href="#">Another action</a></li>--}}
-                           {{--<li><a href="#">Something else here</a></li>--}}
-                           {{--<li class="divider"></li>--}}
-                           {{--<li><a href="#">Separated link</a></li>--}}
-                       {{--</ul>--}}
-                   {{--</div><!-- /btn-group -->--}}
-                   {{--<input type="text" class="form-control">--}}
-               {{--</div>--}}
-               {{---------------------------------------}}
+
                <div class="col-sm-8">
 
                   <div class="col-sm-3">
@@ -48,10 +35,6 @@
                     {{ Form::select('lib_book_publisher_id', $lib_book_publisher_id, Input::old('lib_book_publisher_id'), array('class' => 'form-control')) }}
                   </div>
 
-                  {{--<div class="col-sm-3">--}}
-                    {{--{{ Form::label('title', 'Book Title') }}--}}
-                    {{--{{ Form::select('title', $lib_book_id, Input::old('title'), array('class' => 'form-control')) }}--}}
-                  {{--</div>--}}
                   <br><br>
                   {{ Form::submit('Search', array('class'=>' pull-left btn btn-success btn-xs','id'=>'button'))}}
                </div>
@@ -76,6 +59,7 @@
                            <th>Book Type</th>
                            <th>Price(TK.)</th>
                            <th>Is Rented?</th>
+                           <th>Commercial</th>
                            <th>Action</th>
                         </tr>
                  </thead>
@@ -85,7 +69,7 @@
                               <tr>
                                   <td><input type="checkbox" name="id[]" class="myCheckbox" value=""></td>
                                   <td>
-                                  <a href="{{ URL::route('amw.view-exm-courses') }}"
+                                  <a href=""
                                   class="btn-link" title="View Details" style="color:#800080">{{$list->title}}
                                   </a>
                                   </td>
@@ -97,19 +81,24 @@
                                   <td>{{$list->book_type}}</td>
                                   <td>{{$list->book_price}}</td>
                                   <td>{{$list->is_rented}}</td>
+                                  <td>{{ucfirst($list->commercial)}}
+                                  </td>
                                   <td>
                                       {{--<a href="" class="btn btn-default btn-xs" data-toggle="modal" data-target="#book" title="Show" style="font-size: 11px;color: darkmagenta"><span class="fa fa-eye"></span></a>--}}
-                                      @if($list->is_rented == 'yes')
-                                        <a href="{{ URL::route('faculty.add-book-to-cart',['book_id'=>$list->id]) }}" id="addCart" onclick="testAjax()"
-                                        class="btn-link" title="Add Book" style="color:darkblue"><b><ins>Add To Cart</ins></b>
-                                        </a>
+                                      @if($list->commercial == 'no')
+                                      <a href="{{ URL::route('student.book.download') }}"
+                                      class="btn-link" title="download" style="color:#8b0835"><b><i class="fa fa-download"></i> <ins>Download</ins></b>
+                                      </a>
+
+                                      @else
+                                          <a href="{{ URL::route('faculty.add-book-to-cart',['book_id'=>$list->id]) }}" id="addCart" onclick="testAjax()"
+                                          class="btn-link" title="Add Book" style="color:darkblue"><b><i class="fa fa-shopping-cart"></i> <ins>Add To Cart</ins></b>
+                                          </a>
                                       @endif
                                       {{--<a data-href="{{URL::route('examination.amw.delete-exam-data', ['id'=>$exam_list->id]) }}" class="btn btn-xs btn-default" data-toggle="modal" data-target="#confirm-delete" style="font-size: 12px;color: lightcoral"><span class="fa  fa-trash-o"></span></a>--}}
                                   </td>
                               </tr>
                          @endforeach
-                         {{--<a class="pull-right btn btn-sm btn-info" href="{{ URL::route('amw.exam-list') }}"  style="color: #ffffff" ><b>All</b></a>--}}
-
                      @endif
                  </tbody>
               </table>
