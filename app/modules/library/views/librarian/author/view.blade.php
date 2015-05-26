2d356efa2df5cf5ab50b1c992ce3c3335a69737c
ratnaakter <ratna@edutechsolutionsbd.com>
2015-05-26 10:26:19 +0600

    Issue #109

--- app/modules/library/views/librarian/author/view.blade.php
+++ app/modules/library/views/librarian/author/view.blade.php
@@ -1,6 +1,6 @@
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal">{{HTML::image('assets/icon/media-close-btn.png')}}</button>
-    <h4 class="modal-title" style="text-align: center;color: #800080;font-size: x-large">View Book Author :: {{isset($view_author->name) ? $view_author->name :''}} Information</h4>
+    <h4 class="modal-title" style="text-align: center;color: #800080;">View Book Author :: {{isset($view_author->name) ? $view_author->name :''}} Information</h4>
 </div>
 <div class="modal-body">
     <div style="padding: 10px;">
