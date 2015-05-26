c538cd4f828d21b2dc9966199a58eca36bd555ff
shafiqul haque <shafi@edutechsolutionsbd.com>
2015-05-26 16:33:07 +0600

    Issue#112 update

--- app/modules/library/views/show_cart.blade.php
+++ app/modules/library/views/show_cart.blade.php
@@ -1,10 +1,25 @@
 <!-- Show cart info    -->
 
+@if ( 4 ==  Auth::user()->get()->role_id)
+    @if(isset($all_cart_books))
 
-@if(isset($all_cart_books))
+      <div>
+          <a href="{{ URL::route('student.view-cart') }}" class="pull-right btn button-large bg-danger"  style="color: navy" ><i class="fa fa-large fa-shopping-cart"></i>
+          <strong class="img-circle"><span class="label label-primary">{{count($all_cart_books)}}</span></strong></a>
+      </div>
+
+    @endif
 
 
-  <a class="container"  href="{{ URL::route('faculty.view-cart')}}">{{HTML::image('/img/cartss.gif')}}<b style="margin-left: 1019px"><span class="label label-success">{{count($all_cart_books)}}</span></b></a>
+
+@elseif( 2 ==  Auth::user()->get()->role_id )
+
+    @if(isset($all_cart_books))
+
+
+      <a class="container"  href="{{ URL::route('faculty.view-cart')}}">{{HTML::image('/img/cartss.gif')}}<b style="margin-left: 1019px"><span class="label label-success">{{count($all_cart_books)}}</span></b></a>
+    @endif
+
 @endif
 
 <style>
@@ -16,3 +31,4 @@
     border-radius: 38%;
 }
 </style>
+
