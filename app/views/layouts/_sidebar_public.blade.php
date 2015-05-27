943550f73a3a3ebc033fecb58e472a1ccf8651af
Almas Hossain <almas@edutechsolutionsbd.com>
2015-05-27 16:15:50 +0600

    Issue #103

--- app/views/layouts/_sidebar_public.blade.php
+++ app/views/layouts/_sidebar_public.blade.php
@@ -24,29 +24,14 @@
             <i class="fa fa-angle-double-right"></i> <span>Applicant Login</span>
         </a>
     </li>
-
-    {{--<li class="treeview">
-        <a href="#">
-            <i class="fa fa-leaf" style="color: #09b021"></i>
-            <span>Common</span>
-            <i class="fa fa-angle-left pull-right"></i>
-        </a>
-        <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-angle-double-right"></i> Common</a></li>
-        </ul>
-    </li>--}}
-
-    <!--<li class="treeview">
+    <li class="treeview">
         <a href="#">
             <i class="fa fa-download" style="color: #803a0f"></i>
             <span>Admission</span>
             <i class="fa fa-angle-left pull-right"></i>
         </a>
         <ul class="treeview-menu">
-            <li class="active"><a tabindex="-1" a href="{{ URL::route('admission.public.degree_offer_list') }}"></i><i class="fa fa-globe" style="color:#0089db"></i>Apply For Admission Test</a></li>
+            <li class="active"><a tabindex="-1" a href="{{ URL::route('admission.public.degree_offer_list') }}"></i><i class="fa fa-globe" style="color:#0089db"></i>Admission Degree Offer</a></li>
         </ul>
-
-    </li>-->
-
-
+    </li>
 </ul>
