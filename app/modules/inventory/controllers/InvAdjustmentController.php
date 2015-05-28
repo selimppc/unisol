49121a21931aa7802a0bfb0df35f9584ab8b3b7f
Selim Reza <selim@edutechsolutionsbd.com>
2015-05-28 13:45:59 +0600

    Issue #117 added : stock adjustment

--- app/modules/inventory/controllers/InvAdjustmentController.php
+++ app/modules/inventory/controllers/InvAdjustmentController.php
@@ -5,7 +5,7 @@ class InvAdjustmentController extends \BaseController {
 	public function index_stock_adjustment(){
         $pageTitle = "Stock Adjustment ";
         $data = InvStoc::get();
-        return View::make('inventory::stock.stock_view', compact('data','pageTitle'));
+        return View::make('inventory::adjust_head.index', compact('data','pageTitle'));
     }
 
 
