43189b25ef8c08fa8bb7ca4aa64194b5a98bf309
shafiqul haque <shafi@edutechsolutionsbd.com>
2015-05-31 12:32:07 +0600

    Issue#119 updates

---
+++ app/modules/rnc/controllers/RnCAmwController.php
@@ -0,0 +1,55 @@
+<?php
+
+class RnCAmwController extends \BaseController
+{
+
+    function __construct()
+    {
+        $this->beforeFilter('', array('except' => array('')));
+    }
+
+
+	public function index()
+	{
+		//
+	}
+
+
+
+	public function create()
+	{
+		//
+	}
+
+
+	public function store()
+	{
+		//
+	}
+
+
+	public function show($id)
+	{
+		//
+	}
+
+
+	public function edit($id)
+	{
+		//
+	}
+
+
+	public function update($id)
+	{
+		//
+	}
+
+
+	public function destroy($id)
+	{
+		//
+	}
+
+
+}
