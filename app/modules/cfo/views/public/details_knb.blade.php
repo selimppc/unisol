cb9945633d84571c43b2ad57fa0ee96059f28207
Almas Hossain <almas@edutechsolutionsbd.com>
2015-06-04 12:47:42 +0600

    issue # 118 KB rating

--- app/modules/cfo/views/public/details_knb.blade.php
+++ app/modules/cfo/views/public/details_knb.blade.php
@@ -8,21 +8,47 @@
 
 @section('content')
 
+{{ HTML::script('assets/etsb/etsb_js/jquery/jquery.min.js')}}
+{{ HTML::script('assets/etsb/etsb_js/jquery-ui/jquery-ui.min.js')}}
+
+<script>
+    $(function() {
+        $('.ratings_stars').hover(
+                // Handles the mouseover
+                function (e) {
+                    $(this).prevAll().andSelf().addClass('ratings_over');
+                    $(this).nextAll().removeClass('ratings_vote');
+                },
+                // Handles the mouseout
+                function (e) {
+                    $(this).prevAll().andSelf().removeClass('ratings_over');
+                    set_votes($(this).parent());
+                }
+        );
+    });
+</script>
 <h4><b>Knowledge Base : <em style="font-size: medium;color: #0490a6">{{$title}}</em></b> </h4>
 
 <div class="row">
    <div class="box box-solid">
        <div class="box-body">
-          <div class="rating" style="font-size: x-large;color:darkorange">
-               <span class="ratings_stars" title="vote 1">☆</span>
-               <span class="ratings_stars" title="vote 2">☆</span>
-               <span class="ratings_stars" title="vote 3">☆</span>
-               <span class="ratings_stars" title="vote 4">☆</span>
-               <span class="ratings_stars" title="vote 5">☆</span>
-          </div>
              <table id="" class="table table-striped  table-bordered" >
                 <tbody>
                     @if(isset($data))
+                        <tr>
+                            <td>
+                                <div class='kb_choice'>
+                                    <div id="r1" class="rate_kb">
+                                        <div class="star_1 ratings_stars"></div>
+                                        <div class="star_2 ratings_stars"></div>
+                                        <div class="star_3 ratings_stars"></div>
+                                        <div class="star_4 ratings_stars"></div>
+                                        <div class="star_5 ratings_stars"></div>
+                                        <div class="total_votes">vote data</div>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
                          <tr>
                           <td>
                              {{$data->description}}
@@ -35,43 +61,43 @@
    </div>
 </div>
 
-<style>
-
-
-</style>
-
-<script>
- /*$('.ratings_stars').hover(
-     // Handles the mouseover
-     function() {
-         $(this).prevAll().andSelf().addClass('ratings_over');
-         $(this).nextAll().removeClass('ratings_vote');
-     },
-     // Handles the mouseout
-     function() {
-         $(this).prevAll().andSelf().removeClass('ratings_over');
-         set_votes($(this).parent());
-     }
- );*/
-</script>
 
 
 <style>
-.rating span:hover:before {
-       content: "\2605";
-       position: absolute;
+    .rate_kb {
+        border:     1px solid #CCC;
+        overflow:   visible;
+        padding:    10px;
+        position:   relative;
+        width:      190px;
+        height:     52px;
     }
-    .rating span {
-      display: inline-block;
-      position: relative;
-      width: 1.1em;
+    .ratings_stars {
+        background: url("{{ URL::asset('assets/images/star_empty.png') }}") no-repeat;
+        float:      left;
+        height:     28px;
+        padding:    0px;
+        width:      32px;
     }
-    .rating span:hover:before,
-    .rating span:hover ~ span:before {
-       content: "\2605";
-       position: absolute;
+    .ratings_vote {
+        background: url("{{ URL::asset('assets/images/star_full.png') }}") no-repeat;
+    }
+    .ratings_over {
+        background: url("{{ URL::asset('assets/images/star_highlight.png') }}") no-repeat;
     }
 
+    .total_votes {
+        background: #eaeaea;
+        top: 58px;
+        left: 0;
+        padding: 5px;
+        position:   absolute;
+    }
+    .kb_choice {
+        font: 10px verdana, sans-serif;
+        margin: 0 auto 40px auto;
+        width: 180px;
+    }
 </style>
 
 
