def65fbdfff4d2d77ed928386e407937f2419f68
Selim Reza <selim@edutechsolutionsbd.com>
2015-06-07 11:09:14 +0600

    Issue #126 GL models

---
+++ app/modules/accounts/models/AccVoucherDetail.php
@@ -0,0 +1,68 @@
+<?php
+//use Carbon\Carbon;
+
+class AccVoucherDetail extends \Eloquent
+{
+    //TODO :: model attributes and rules and validation
+    protected $table = 'acc_voucher_detail';
+    protected $fillable = [
+        'acc_voucher_head_id', 'acc_chart_of_accounts_id', 'sub_account_code',
+        'currency_id', 'exchange_rate', 'prime_amount',
+        'base_amount', 'note'
+    ];
+
+    private $errors;
+    private $rules = [
+        'acc_voucher_head_id' => 'required|integer',
+        'acc_chart_of_accounts_id' => 'required|integer',
+        //'acm_marks_distribution_id' => 'required|integer',
+        //'acm_class_schedule_id' => 'required|integer',
+        //'status' => 'required|integer',
+    ];
+
+    public function validate($data)
+    {
+        $validate = Validator::make($data, $this->rules);
+        if ($validate->fails())
+        {
+            $this->errors = $validate->errors();
+            return false;
+        }
+        return true;
+    }
+    public function errors()
+    {
+        return $this->errors;
+    }
+
+
+    //TODO : Model Relationship
+
+
+
+    // TODO : user info while saving data into table
+    public static function boot(){
+        parent::boot();
+        static::creating(function($query){
+            if(Auth::user()->check()){
+                $query->created_by = Auth::user()->get()->id;
+            }
+        });
+        static::updating(function($query){
+            if(Auth::user()->check()){
+                $query->updated_by = Auth::user()->get()->id;
+            }
+        });
+    }
+
+
+    //TODO : Scope Area
+
+    public function geDateAttribute($date) {
+        return Carbon::parse($date)->format('d-M-Y'); //Change the format to whichever you desire
+    }
+
+
+
+
+}
