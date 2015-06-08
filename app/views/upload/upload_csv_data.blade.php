1a51aa40fe7c642ad71993ebb19f7fde5bcdf592
Selim Reza <selim@edutechsolutionsbd.com>
2015-06-08 17:35:30 +0600

    upload csv data into mysql table

--- app/views/upload/upload_csv_data.blade.php
+++ app/views/upload/upload_csv_data.blade.php
@@ -7,7 +7,7 @@
 <div class="modal-content">
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-     <h4> Upload CSV Data into Mysql Table </h4>
+     <h4> Upload CSV Data into Mysql Table (Chart of Accounts) </h4>
 </div>
 
 <div style="padding: 2%; width: 99%;">
