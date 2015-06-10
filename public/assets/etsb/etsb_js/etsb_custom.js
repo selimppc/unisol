2a7724dd2511d1ceed8b4593f750ab56b187dee1
Selim Reza <selim@edutechsolutionsbd.com>
2015-06-10 10:39:43 +0600

    updated date picker js in application

--- public/assets/etsb/etsb_js/etsb_custom.js
+++ public/assets/etsb/etsb_js/etsb_custom.js
@@ -2,46 +2,30 @@
  * Created by SELIM on 3/18/2015.
  */
 
-$(function() {
-    $('.date-picker').each(function() {
+(function () {
+    'use strict';
+    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+        var msViewportStyle = document.createElement('style')
+        msViewportStyle.appendChild(
+            document.createTextNode(
+                '@-ms-viewport{width:auto!important}'
+            )
+        )
+        document.querySelector('head').appendChild(msViewportStyle)
+    }
+})();
 
-        var $picker = $(this);
-        $picker.datepicker({
-            format:'yyyy-mm-dd'
-        });
-        var pickerObject = $picker.data('date-picker');
 
-        $picker.on('changeDate', function(ev){
-            $picker.datepicker('hide');
-        });
+//date_picker
+$('.date_picker').each(function() {
+
+    var $picker = $(this);
+    $picker.datepicker({
+        format:'yyyy-mm-dd'
     });
-    /*$('.date-picker').datepicker().next().on(ace.click_event, function(){
-     $(this).prev().focus();
-     var $picker = $(this);
-     $picker.datepicker({
-     format:'yyyy-mm-dd'
-     });
-     var pickerObject = $picker.data('date_picker');
+    var pickerObject = $picker.data('date_picker');
 
-     $picker.on('changeDate', function(ev){
-     $picker.datepicker('hide');
-     });
-     });*/
+    $picker.on('changeDate', function(ev){
+        $picker.datepicker('hide');
+    });
 });
-
-/*
-$(function() {
-    $("#example1").dataTable({
-        "bPaginate": false,
-        "bSort": true,
-        "bInfo": true
-    });
-    $('#example2').dataTable({
-        "bPaginate": true,
-        "bLengthChange": false,
-        "bFilter": false,
-        "bSort": true,
-        "bInfo": true,
-        "bAutoWidth": false
-    });
-});*/
