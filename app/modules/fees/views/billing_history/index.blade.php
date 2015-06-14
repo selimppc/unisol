c637a3d5c9526d0adb008c5f55ca9e586330f118
ratnaakter <ratna@edutechsolutionsbd.com>
2015-06-14 18:06:42 +0600

    Issue #116

--- app/modules/fees/views/billing_history/index.blade.php
+++ app/modules/fees/views/billing_history/index.blade.php
@@ -10,7 +10,7 @@
         <div class="col-md-12">
             <h3 class="text-purple ">Fees::Billing History</h3>
             <div class="help-text-top">
-                You can view all lists of Billing History and search. Also this panel will allow you to perform some actions Like <b>View</b> individual billing history under <b>Action</b>.
+                You can view all lists of Billing History and search. Also this panel will allow you to perform some actions Like <b>View</b> individual billing history under the column <b>Action</b>.
             </div><!-- /.box-body -->
         </div><!-- ./col -->
     </div><!-- /.row -->
@@ -18,7 +18,7 @@
         <div class="col-md-12">
             <div class="nav-tabs-custom">
                 <ul class="nav nav-tabs">
-                    <li class="active"><a href="#tab_1" data-toggle="tab">Billing Setup</a></li>
+                    <li class="active"><a href="#tab_1" data-toggle="tab">Billing History</a></li>
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                             Settings  <span class="caret"></span>
@@ -41,7 +41,7 @@
 
                         {{--****************** Filter :Starts ***********************--}}
                         <div>
-                          {{--  {{Form::open(array('route'=> ['billing.setup']))}}--}}
+                            {{Form::open(array('route'=> ['billing.history']))}}
                             <div class="col-sm-8">
                                 <div class="col-sm-4">
                                     {{ Form::label('department_id', 'Department') }}
@@ -55,53 +55,59 @@
                                     {{ Form::label('batch_id', 'Batch') }}
                                     {{ Form::select('batch_id',$batch, Input::old('batch_id'), array('class' => 'form-control')) }}
                                 </div>
-                                <div class="col-sm-2">
-                                    </br>
-                                    {{ Form::submit('Filter', array('class'=>'btn btn-success btn-xs','id'=>'button'))}}
+
+                                <div class="col-sm-4">
+                                    <div class="form-inline radio-inline">
+                                        <div class="radio radio-style">
+                                            {{ Form::radio('studentOrApplicant', 'student', true) }}
+                                            {{ Form::label('student', 'Student') }}
+                                        </div>
+                                        <div class="radio">
+                                            {{ Form::radio('studentOrApplicant', 'applicant') }}
+                                            {{ Form::label('applicant', 'applicant') }}
+                                        </div>
+                                     </div>
+                                    <div>{{ Form::text('student_id',Input::old('student_id'), array('placeholder'=>'Enter id')) }}</div></br>OR
+                                    <div>{{ Form::text('student_name',Input::old('student_name'), array('placeholder'=>'Enter name')) }}</div></br>
+
+                                {{ Form::submit('Filter', array('class'=>'btn btn-success','id'=>'button'))}}
                                 </div>
                             </div>
-                            {{--{{Form::close()}}--}}
+                            {{Form::close()}}
                         </div>
                         {{--*****************Filter :Ends ****************************--}}
-
+                   {{-- For student--}}
                         <div class="box-body table-responsive ">
                             <table id="example" class="table table-bordered table-hover table-striped">
                                 <thead>
                                 <tr>
+                                    <th>Student</th>
                                     <th>Schedule</th>
-                                    <th>Item</th>
-                                    <th>Cost</th>
-                                    <th>Deadline</th>
-                                    <th>Fined Cost</th>
+                                    <th>Amount</th>
                                     <th>Action</th>
                                 </tr>
                                 </thead>
                                 <tbody>
-                                @if(isset($data))
-                                    @foreach($data as $value)
-                                        <tr>
-                                            <td><input type="checkbox" name="id[]"  class="myCheckbox" value="{{ $value->id }}">
-                                            </td>
-                                            <td>{{isset($value->scheduleTitle) ? $value->scheduleTitle:''}}</td>
 
-                                            <td>{{isset($value->billingTitle) ? $value->billingTitle: ''}}</td>
+                                </tbody>
+                            </table>
+                            {{ Form::close() }}
+                        </div>
 
-                                            <td>{{isset($value->cost) ? $value->cost : ''}}</td>
 
-                                            <td>{{date("d-m-Y", strtotime((isset($value->deadline)) ? $value->deadline : '') ) }}</td>
+                        {{-- For Applicant--}}
+                        <div class="box-body table-responsive ">
+                            <table id="example" class="table table-bordered table-hover table-striped">
+                                <thead>
+                                <tr>
+                                    <th>Name of Applicant</th>
+                                    <th>Schedule</th>
+                                    <th>Amount</th>
+                                    <th>Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
 
-                                            <td>{{isset($value->fined_cost) ? $value->fined_cost : ''}}</td>
-
-                                            <td>
-                                                <a href="{{ URL::route('billing.setup.view',['id'=>$value->id])}}" class="btn btn-xs btn-default" data-toggle="modal" data-target="#showModal" href=""><i class="fa fa-eye" style="color: green"></i></a>
-
-                                                <a href="{{ URL::route('billing.setup.edit',['id'=>$value->id])}}" class="btn btn-xs btn-default" data-toggle="modal" data-target="#editModal" href="" ><i class="fa fa-pencil-square-o" style="color: #0044cc"></i></a>
-
-                                                <a data-href="{{URL::route('billing.setup.delete', ['id'=>$value->id]) }}" class="btn btn-xs btn-default" data-toggle="modal" data-target="#confirm-delete" style="font-size: 12px;color: lightcoral"><span class="fa  fa-trash-o"></span></a>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                @endif
                                 </tbody>
                             </table>
                             {{ Form::close() }}
