9b2d90a138c491202ef9e4ce6f95097842e111c2
shafiqul haque <shafi@edutechsolutionsbd.com>
2015-06-17 11:15:47 +0600

    Issue#133 updates

--- app/modules/rnc/views/faculty/research_paper/r_p_w_f/_w_f_form.blade.php
+++ app/modules/rnc/views/faculty/research_paper/r_p_w_f/_w_f_form.blade.php
@@ -10,6 +10,9 @@
 
 {{Form::hidden('rnc_research_paper_id', $rnc_r_p_id,['id'=>'research-paper-id'])}}
 
+<div id="test-edit"></div>
+
+
 <div class='row'>
        <div class="col-sm-4">
         <div class='form-group'>
@@ -58,7 +61,7 @@
             <td id="new-column-value-{{ $model_value->id }}">{{ $model_value->relRnCWriterBeneficial->value }}</td>
             <td>
                 <a data-href="{{ $model_value->id }}" data-benf="{{$model_value->relRnCWriterBeneficial->id}}" class="btn btn-default btn-sm delete-dt-2" id="delete-dt-2{{ $model_value->id }}" ><i class="fa fa-trash-o" style="font-size: 15px;color: red"></i></a>
-                <a href="{{ URL::route('faculty.research-paper-writer-beneficial.edit', ['rnc_r_p_id'=>$rnc_r_p_id ,'id'=>$model_value->id , 'ben_id'=>$model_value->relRnCWriterBeneficial->id]) }}" class="btn btn-sm btn-default" data-toggle="modal" data-target="#myModal2" href="" ><i class="fa fa-pencil-square-o" style="color: #0044cc" title="Edit"></i></a>
+                <a class="btn btn-sm btn-default" id="edit-writer-and-beneficial"><i class="fa fa-pencil-square-o" style="color: #0044cc" title="Edit"></i></a>
             </td>
         </tr>
       <?php $counter++;?>
@@ -75,18 +78,18 @@
 {{ HTML::script('assets/etsb/etsb_js/jquery-ui/jquery-ui.min.js')}}
 
 <script type="text/javascript">
- $(function(){
-     $( "#search_writer_name" ).autocomplete({
-      source: "/rnc/ajax/fac-get-writer-name-auto-complete",
-      minLength: 1,
-      select: function(event, ui) {
-        $('#select_writer_name').val(ui.item.label);
-        $('#wr-name-id').val(ui.item.writer_user_id);
-        $('#writer-name').val(ui.item.label);
-      }
- });
+$(function(){
+         $( "#search_writer_name" ).autocomplete({
+              source: "/rnc/ajax/fac-get-writer-name-auto-complete",
+              minLength: 1,
+              select: function(event, ui) {
+                $('#select_writer_name').val(ui.item.label);
+                $('#wr-name-id').val(ui.item.writer_user_id);
+                $('#writer-name').val(ui.item.label);
+              }
+         });
 
-//Product Add(s) : ok
+    //Beneficial Add(s) : ok
      $tableItemCounter = 0; //To stop additem if exist
      var $arrayRnc = []; //To stop additem if exist
 
@@ -139,27 +142,87 @@
                  }
              }
          }
- 	});
+ 	 });
 
 
-//delete : ok
-	$(function(){
-          $('.delete-dt-2').click(function(e) {
-            e.preventDefault();
-            var $btn = $(this);
-            $.ajax({
-                url: '/rnc/faculty/research-paper-writer-beneficial/ajax-delete-req-detail',
-                type: 'POST',
-                dataType: 'json',
-                data: { id:  $(this).data("href"), ben_id: $(this).data("benf") },
-                success: function(response)
-                {
-                    $btn.closest("tr").remove();
-                    $('#something-delete').html(response);
-                }
-            });
-          });
-       });
+    //delete : ok
+
+     $('.delete-dt-2').click(function(e) {
+        e.preventDefault();
+        var $btn = $(this);
+        $.ajax({
+            url: '/rnc/faculty/research-paper-writer-beneficial/ajax-delete-req-detail',
+            type: 'POST',
+            dataType: 'json',
+            data: { id:  $(this).data("href"), ben_id: $(this).data("benf") },
+            success: function(response)
+            {
+                $btn.closest("tr").remove();
+                $('#something-delete').html(response);
+            }
+        });
+     });
+
+
+
+
+     //Beneficial Edit(s)
+          $tableItemCounter = 0; //To stop additem if exist
+          var $arrayRnc = []; //To stop additem if exist
+
+          $("#edit-writer-and-beneficial").click(function(event){
+              $res_pap_id = $("#research-paper-id").val();
+              $writer_id = $("#wr-name-id").val();
+              //$writer_name_val = $("#writer_name").val();
+              $benfcl_val = $("#beneficial-value").val();
+              $wr_name = $("#writer-name").val();
+              var total_count_share = "<?php echo $cal_benefit_share; ?>";
+
+              if($writer_id == null || $benfcl_val == null ){
+                  alert("please add Writer Name and Beneficial then try Again!");
+                  return false;
+              }else{
+                  $wrtr_u_id = $("#wr-name-id").val(); // writer user ID
+                  var index = $.inArray($wrtr_u_id, $arrayRnc);
+                  if (index>=0) {
+                      alert("You already added this Writer in the below table");
+                      //also flash the existing text field
+                      $("#writer-name").val("");
+                      $("#wr-name-id").val("");
+                      $("#beneficial-value").val("");
+                      $("#writer-name").val("");
+                      $("#search_writer_name").val("");
+
+                      return false;
+                  } else {
+
+                      var $total_ben = parseInt(total_count_share) + parseInt($benfcl_val);
+
+                      if($total_ben > 100){
+                         $("#beneficial-value").val("");
+                             alert( $total_ben +'  ,'+ 'Exceeded the Total share 100%. Please decrease your share percentage');
+                         $("#beneficial-value").focus();
+                         return false;
+                      }else{
+                         $('#test-edit').append("<div> " +
+                               "<input value='"+ $wr_name +"' style='background-color : lavender' readonly> <input type='hidden' name='writer_user_id[]' value='"+ $writer_id +"' >" +
+                               "<input name='value[]' value='"+ $benfcl_val +"'>" +
+                               "<input type='button' class='pull-right btn-xs btn-linkedin' value='Update'>" +
+                           " </div>");
+
+                         $arrayRnc.push($wrtr_u_id);
+
+                         //flush the input fields
+                         $("#writer-name").val("");
+                         $("#beneficial-value").val("");
+                         $("#writer-name").val("");
+                         $("#search_writer_name").val("");
+                      }
+                  }
+              }
+      	 });
+
+
 });
 
 </script>
