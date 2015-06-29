96f50bd861a55dabc9eb6fd67fbcfe7f81046639
Selim Reza <selim@edutechsolutionsbd.com>
2015-06-29 15:24:31 +0600

    Iuuse #134

--- app/modules/payment/views/account_payable/_script.blade.php
+++ app/modules/payment/views/account_payable/_script.blade.php
@@ -22,17 +22,43 @@ $(function() {
 
          //Get Table Data
          $invoice_code = $.trim(tableData[0]);
-         $amount = $.trim(tableData[1]);
+         $amount = Math.round($.trim(tableData[1]));
          $date = $.trim(tableData[2]);
-
 
          var index = $.inArray($invoice_code, $arrayItems);
          if (index>=0) {
             alert("You already added this Invoice");
             return false;
          }else{
-            $('#new-data').append("<tr> <td><input name='invoice[]' value='"+$invoice_code+"' readonly> <input name='inv_product_id[]' type='hidden' value='"+$invoice_code+"'></td>  <td><input name='amount[]' value='"+$amount+"' readonly></td> </tr>");
-            $arrayItems.push($invoice_code);
+            var $pay_amount = Math.round($('#pay-for-amount').val());
+            if($pay_amount == null || $pay_amount == 0){
+                alert("In-sufficient Amount!");
+                return false;
+            }else{
+                if($pay_amount > $amount){
+                    //Payable Amount
+                    var $least_pay_amount = (Math.round($pay_amount) - Math.round($amount));
+                    $('#pay-for-amount').val($least_pay_amount);
+                    $pr_amount = Math.round($amount);
+
+                    $('#new-data').append("<tr> <td><input name='invoice[]' value='"+$invoice_code+"' readonly> <input name='inv_product_id[]' type='hidden' value='"+$invoice_code+"'></td>  <td><input name='amount[]' value='"+$pr_amount+"' readonly></td> </tr>");
+                    $arrayItems.push($invoice_code);
+
+                }else if($pay_amount < $amount){
+                    //Payable Amount
+                    var $least_pay_amount = (Math.round($amount) - Math.round($pay_amount));
+                    $('#pay-for-amount').val('0');
+                    $pr_amount = Math.round($pay_amount);
+
+                    $('#new-data').append("<tr> <td><input name='invoice[]' value='"+$invoice_code+"' readonly> <input name='inv_product_id[]' type='hidden' value='"+$invoice_code+"'></td>  <td><input name='amount[]' value='"+$pr_amount+"' readonly></td> </tr>");
+                    $arrayItems.push($invoice_code);
+
+                }
+            }
+            //total amount in the area of allocation
+            var $total_amount_allocation = $('#allocation-total-amount').val();
+            var $total_allocation_amt = (Math.round($total_amount_allocation) + Math.round($pr_amount));
+            $('#allocation-total-amount').val($total_allocation_amt);
          }
 
 
