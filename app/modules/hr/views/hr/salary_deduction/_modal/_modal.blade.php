f7893b40b51dc4ebd142772f6ee64d1a137c9f45
shafiqul haque <shafi@edutechsolutionsbd.com>
2015-06-30 13:34:27 +0600

    Issue#153 updates

---
+++ app/modules/hr/views/hr/salary_deduction/_modal/_modal.blade.php
@@ -0,0 +1,38 @@
+<!-- Add Modal -->
+<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog" style="z-index:1050">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">HR Loan Detail of <strong>
+{{--        {{ $loan_head_name->relHrLoanHead->title }}--}}
+        </strong></h4>
+
+      </div>
+      <div class="modal-body">
+        @include('hr::hr.loan_detail._form')
+      </div>
+    </div>
+  </div>
+</div>
+
+
+{{-- Modal for delete --}}
+<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Confirm Delete</h4>
+            </div>
+            <div class="modal-body">
+                <strong>Are you sure to delete?</strong>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                <a href="#" class="btn btn-danger danger">Delete</a>
+
+            </div>
+        </div>
+    </div>
+</div>
