cb6c6b6c21a5fa486ce1925263c441a10420d0ff
tanin <tanin@edutechsolutionsbd.com>
2015-07-01 16:05:29 +0600

    Issue #155 work week crud

--- app/modules/hr/views/hr/leave/_form.blade.php
+++ app/modules/hr/views/hr/leave/_form.blade.php
@@ -9,8 +9,39 @@
           {{ Form::select('hr_leave_type_id', $leave_type_id, Input::old('hr_leave_type_id'), array('class' => 'form-control','required'=>'required')) }}
     </div>
 
+    <div class='form-group'>
+       {{ Form::label('reason', 'Reason') }}
+       {{ Form::text('reason', Input::old('reason'),['class'=>'form-control', 'required']) }}
+    </div>
+
+    <div class="form-group">
+        {{ Form::label('leave_duration', 'leave Duration') }}
+            <div>{{ Form::select ('leave_duration',  array('' => 'Select one',
+                'full-day' => 'Full Day', 'half-day' => 'Half Day'), Input::old('leave_duration'),
+                 array('class' => 'form-control ','required')) }}</div>
+            </div>
+    </div>
+
+    <div class='form-group'>
+        <div class="col-lg-6" style="padding-left: 1;">
+           {{ Form::label('from_date', 'Start Date') }}
+           {{ Form::text('from_date',  Input::old('from_date'),['class'=>'form-control date_picker']) }}
+        </div>
+
+        <div class="col-lg-6" style="padding-right: 1;">
+           {{ Form::label('to_date', 'End Date') }}
+           {{ Form::text('to_date',  Input::old('to_date'),['class'=>'form-control date_picker']) }}
+        </div>
+    </div>
+
+    <div class='form-group'>
+        {{ Form::label('alt_contact_no', 'Alt Contact No') }}
+        {{ Form::text('alt_contact_no', Input::old('alt_contact_no'),['class'=>'form-control', 'required']) }}
+    </div>
 
 
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
     {{ Form::submit('Save', array('class'=>'pull-right btn btn-info')) }}
     <a href="" class="pull-right btn btn-default" style="margin-right: 5px">Close</a>
 
--- app/views/layouts/_sidebar_hr.blade.php
+++ app/views/layouts/_sidebar_hr.blade.php
@@ -17,7 +17,9 @@
              <li><a href="{{ URL::to('hr/employee') }}"></i><i class="fa fa-user" style="color: #0707cc"></i>HR Employee</a></li>
              <li><a href="{{ URL::to('hr/currency') }}"></i><i class="fa fa-dollar" style="color: #ff1383"></i>Currency</a></li>
              <li><a href="{{ URL::to('hr/allowance') }}"></i><i class="fa fa-sort" style="color: #00fff2"></i>HR Allowance</a></li>
-             <li><a href="{{ Route('attendance') }}"></i><i class="fa fa-sort" style="color: #00fff2"></i>HR Attendance</a></li>
+             <li><a href="{{ URL::to('hr/work-week') }}"></i><i class="fa fa-sort" style="color: #00fff2"></i>HR Work Week</a></li>
+             <li><a href="{{ URL::to('hr/leave-type') }}"></i><i class="fa fa-sort" style="color: #00fff2"></i>HR Leave Type</a></li>
+             <li><a href="{{ URL::to('hr/leave') }}"></i><i class="fa fa-sort" style="color: #00fff2"></i>HR Leave </a></li>
 
          </ul>
     </li>
