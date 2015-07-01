062b62adbdf3cb0080ba780281f769cebee8667a
Selim Reza <selim@edutechsolutionsbd.com>
2015-07-01 13:42:12 +0600

    Issue #134

--- app/modules/payment/controllers/AccountPayableController.php
+++ app/modules/payment/controllers/AccountPayableController.php
@@ -68,7 +68,7 @@ class AccountPayableController extends \BaseController {
         $coa_lists = AccChartOfAccounts::lists('description', 'id');
 
         //Unpaid Invoice Lists
-        $unpaid_invoice = AccVUnpaidInvoice::where('supplier_code', $supplier_id)
+        $unpaid_invoice = AccVUnpaidInvoice::where('inv_supplier_id', $supplier_id)
                         //->where('acc_voucher_head_id', $coa_id)->get();
                         ->get();
 
