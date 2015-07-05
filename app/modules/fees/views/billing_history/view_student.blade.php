2fafeae6825e600401384218a52de3bf23b29d65
ratnaakter <ratna@edutechsolutionsbd.com>
2015-07-05 11:42:57 +0600

    Issue #137

---
+++ app/modules/fees/views/billing_history/view_student.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.layout')
+@section('top_menu')
+    @include('layouts._top_menu')
+@stop
+@section('sidebar')
+    @include('layouts._sidebar_amw')
+@stop
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h3 class="text-purple ">User Information</h3>
+        </div><!-- ./col -->
+    </div><!-- /.row -->
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box-body table-responsive ">
+
+                <table class="table table-bordered table-hover table-striped">
+                    <tr>
+                        <td>Name:</td>
+                        <td>{{isset($data->first_name) ? $data->first_name:''}} {{isset($data->last_name) ? $data->last_name:''}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Department:</td>
+                        <td>
+                            {{isset($data->relDepartment->title) ? $data->relDepartment->title :''}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Degree:</td>
+                        <td>
+                            {{isset($data->relDegree->relDegreeProgram->title) ? $data->relDegree->relDegreeProgram->title :''}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Batch:</td>
+                        <td>
+                            {{isset($data->relBatch->batch_number) ? $data->relBatch->batch_number :''}}
+                        </td>
+                    </tr>
+                     <tr>
+                         <td>User Type:</td>
+                         <td>
+                             Student
+                         </td>
+                     </tr>
+                </table>
+            </div>
+            <div>&nbsp;</div>
+                <h3 class="text-purple ">Billing Details Student</h3>
+                <div class="box-body table-responsive ">
+                    <table class="table table-bordered table-hover table-striped">
+                        <thead>
+                        <th>Item</th>
+                        <th>Schedule</th>
+                        <th>Total Amount</th>
+                        <th>Waiver Amount</th>
+                        <th>Amount</th>
+                        </thead>
+                        <tbody>
+                        <?php $i=0; ?>
+                        @if(isset($relation_data))
+                            @foreach($relation_data[0]->relBillingDetailsStudent as $value)
+                                <tr>
+                                    <td>{{$value['relBillingItem']['title']}}</td>
+
+                                    <td>{{$relation_data[$i]->relBillingSchedule->title}}</td>
+
+                                    <td>{{$value['total_amount']}}</td>
+
+                                    <td>{{$value['waiver_amount']}}</td>
+
+                                    <td>{{$value['total_amount']-$value['waiver_amount']}}</td>
+
+                                </tr>
+                            @endforeach
+                        @endif
+                        </tbody>
+                    </table>
+                    <a href="{{ URL::route('billing.history.student')}}" class="btn-link pull-right"><i class="fa fa-backward text-red"></i> Back to All List</a>
+                </div>
+        </div>
+    </div>
+@stop
+
+
+
