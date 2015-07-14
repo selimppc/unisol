d64064ede3263066423fd5e1b9da8ecdde2d0e3a
Selim Reza <selim@edutechsolutionsbd.com>
2015-07-14 14:48:34 +0600

    issue #134

--- app/modules/payment/controllers/AccountReceivableController.php
+++ app/modules/payment/controllers/AccountReceivableController.php
@@ -65,75 +65,123 @@ class AccountReceivableController extends \BaseController {
     }
 
 
+    // Applicant Receivable Voucher
+    public function  applicant_receive_voucher($associated_id, $coa_id){
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+        $data = AccChartOfAccounts::paginate(3);
+        $year_lists = Year::lists('title', 'id');
+        $period_lists = AccChartOfAccounts::list_period();
+        $coa_lists = AccChartOfAccounts::lists('description', 'id');
 
+        //Unpaid Invoice Lists
+        $unpaid_invoice = AccVUnpaidInvApplicant::where('associated_id', $associated_id)
+            //->where('acc_voucher_head_id', $coa_id)->get();
+            ->get();
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+        return View::make('payment::account_receivable.applicant_ar_voucher', compact(
+            'associated_id', 'coa_id', 'unpaid_invoice',
+            'data','year_lists', 'period_lists', 'coa_lists'
+        ));
+    }
 
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
 
+    /*
+     *
+     * Store data into the following 4 table(s) at a time ::
+     * 1. acc_ap_allocation
+     * 2. acc_balance
+     * 3. acc_voucher_detail
+     * 4. acc_voucher_head
+     *
+     */
+    public function store_applicant_ar_voucher(){
+        //Get ALl input Data
+        $input_data = Input::all();
+        $associated_id = Input::get('associated_id');
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+        // Generate Voucher Number
+        $apr_no = AccTrnNoSetup::where('title', '=', "Account Receivable Voucher")
+            ->select(DB::raw('CONCAT (code, LPAD(last_number + 1, 8, 0)) as number'))
+            ->first()->number;
 
+        //Models are here
+        $model_vhead = new AccVoucherHead();
+        $model_vdetail_credit = new AccVoucherDetail();
+        $model_vdetail_debit = new AccVoucherDetail();
+        $model_ap_allocation = new AccApAllocation();
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+        if($model_vhead->validate($input_data)) {
+            DB::beginTransaction();
+            try {
+                //Acc Voucher Head data and Store
+                $data_v_head = [
+                    'voucher_number' => $apr_no,
+                    'type' => "account-receivable-voucher",
+                    'date' => $input_data['date'],
+                    'reference' => "Applicant Money Receipt # ".$apr_no,
+                    'year_id' => $input_data['year_id'],
+                    'period' => $input_data['period'],
+                    'note' => $input_data['note'],
+                    #'status'=> "open",
+                    'status'=> "balanced",
+                ];
+                $model_vhead = $model_vhead->create($data_v_head);
 
+                // Voucher Details Credit Data and Save
+                $data_v_detail_credit = [
+                    'acc_voucher_head_id' => $model_vhead->id,
+                    'acc_chart_of_accounts_id' => $input_data['acc_chart_of_accounts_id'],
+                    //'associated_id' => $input_data['associated_id'],
+                    'prime_amount' => (-$input_data['total_amount']),
+                    'base_amount' => (-$input_data['total_amount']),
+                    'note'=> "open",
+                ];
+                $model_vdetail_credit->create($data_v_detail_credit);
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+                // Voucher Details Debit Data and Save
+                $data_v_detail_debit = [
+                    'acc_voucher_head_id' => $model_vhead->id,
+                    'acc_chart_of_accounts_id' => $input_data['expense_account'],
+                    'associated_id' => $associated_id, //$input_data['associated_id'],
+                    'prime_amount' => $input_data['total_amount'],
+                    'base_amount' => $input_data['total_amount'],
+                    'note'=> "open",
+                ];
+                $model_vdetail_debit->create($data_v_detail_debit);
+
+                // Insert Allocation Invoice data and Store to ACC_AP_ALLOCATION TABLE
+                for($i = 0; $i < count(Input::get('voucher_head_id')) ; $i++){
+                    $data_ap_allocation[] = [
+                        'voucher_number' => $model_vhead->voucher_number,
+                        'acc_voucher_head_id' => $input_data['voucher_head_id'][$i],
+                        'date' => $input_data['date'],
+                        'prime_amount' => Input::get('amount')[$i],
+                        'base_amount' => Input::get('amount')[$i],
+                    ];
+                }
+                foreach($data_ap_allocation as $values){
+                    $model_ap_allocation->create($values);
+                }
+
+                // Update Acc transaction Number
+                DB::table('acc_trn_no_setup')->where('title', '=', "Account Receivable Voucher")
+                    ->update(array('last_number' => substr($apr_no, 4)));
+
+                //RUN Store procedure
+                DB::select('call sp_acc_voucherpost(?, ?)', array($model_vhead->id, Auth::user()->get()->id ) );
+
+                //Commit the changes
+                DB::commit();
+                Session::flash('message', 'Success !');
+            }catch (Exception $e) {
+                //If there are any exceptions, rollback the transaction`
+                DB::rollback();
+                Session::flash('danger', $e->getMessage() );
+            }
+        }
+        return Redirect::back();
+    }
 
 
 }
