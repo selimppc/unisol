30f60562c4667c2503c33a7dbce8500b6bb8361a
shafiqul haque <shafi@edutechsolutionsbd.com>
2015-07-27 11:36:02 +0600

    Issue#154 updates

--- app/modules/hr/views/hr/salary_transaction/_modal/_modal.blade.php
+++ app/modules/hr/views/hr/salary_transaction/_modal/_modal.blade.php
@@ -34,3 +34,22 @@
         </div>
     </div>
 </div>
+
+{{-- Modal for Confirm salary transaction --}}
+<div class="modal fade" id="confirm-std" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Confirm</h4>
+            </div>
+            <div class="modal-body">
+                <strong>Are you sure to Confirm Transaction?</strong>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                <a href="{{ URL::route('confirm-salary-transaction', ['st_id'=>$values->id ]) }}" class="btn btn-success">OK</a>
+            </div>
+        </div>
+    </div>
+</div>
