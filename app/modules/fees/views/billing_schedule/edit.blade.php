71bbb8f558a5a7a190c303405f60da276e80c2c3
ratnaakter <ratna@edutechsolutionsbd.com>
2015-07-28 14:37:10 +0600

    Issue #137

--- app/modules/fees/views/billing_schedule/edit.blade.php
+++ app/modules/fees/views/billing_schedule/edit.blade.php
@@ -21,8 +21,8 @@
         </div>
 
         <div class="modal-footer">
-            {{ Form::submit('Submit', array('class'=>' btn btn-xs btn-success')) }}
-            <button class=" btn btn-default btn-xs" data-dismiss="modal" type="button">Close</button>
+            {{ Form::submit('Submit', array('class'=>' btn btn-success')) }}
+            <button class=" btn btn-default" data-dismiss="modal" type="button">Close</button>
         </div>
         {{ Form::close() }}
     </div>
