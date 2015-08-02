6392db770d5e9d3f734ecbbc15ccfe1674b5ee89
tanin <tanin@edutechsolutionsbd.com>
2015-08-02 09:31:40 +0600

    user meta-data

--- app/modules/user/views/user_info/meta_data/edit.blade.php
+++ app/modules/user/views/user_info/meta_data/edit.blade.php
@@ -1,1 +1,13 @@
-hello
+
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+    <h4 class="modal-title" id="myModalLabel" style="text-align: center">Edit </h4>
+</div>
+
+<div class="modal-body">
+
+ {{Form::model($model, array('route'=>['user/meta-data/update',$model->id],'class'=>'form-horizontal','files'=>true))}}
+     @include('user::user_info.meta_data._form')
+ {{ Form::close() }}
+
+</div>
