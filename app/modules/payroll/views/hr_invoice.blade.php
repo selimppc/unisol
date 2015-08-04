71bb534a8c3d7db53a77d4d417010b278bd604e5
shafiqul haque <shafi@edutechsolutionsbd.com>
2015-08-04 10:17:47 +0600

    Payroll sidebnar and top menu updates

--- app/modules/payroll/views/hr_invoice.blade.php
+++ app/modules/payroll/views/hr_invoice.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.layout')
+@section('top_menu')
+    @include('layouts._top_menu')
+@stop
 @section('sidebar')
-    @include('layouts._sidebar_amw')
+    @include('layouts._sidebar_hr')
 @stop
 @section('content')
 
