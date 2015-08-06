815fc3600ec02018070a2bedc9cce32765db4dd7
selimppc <me@selimreza.com>
2015-08-06 11:07:33 +0600

    data

--- app/database/migrations/2015_08_05_112528_create_dump_sql.php
+++ app/database/migrations/2015_08_05_112528_create_dump_sql.php
@@ -13,8 +13,19 @@ class CreateDumpSql extends Migration {
 	public function up()
 	{
 
-		#DB::unprepared(file_get_contents("src/sql_dump/coa.sql"));
-		DB::unprepared(file_get_contents("src/sql_dump/codesparam.sql"));
+		DB::unprepared(file_get_contents("src/sql_dump/acc_chart_of_accounts.sql"));
+		DB::unprepared(file_get_contents("src/sql_dump/acc_codesparam.sql"));
+		DB::unprepared(file_get_contents("src/sql_dump/hr_attendance.sql"));
+		DB::unprepared(file_get_contents("src/sql_dump/hr_leave.sql"));
+		DB::unprepared(file_get_contents("src/sql_dump/hr_provident_fund.sql"));
+		DB::unprepared(file_get_contents("src/sql_dump/lib_book_transaction.sql"));
+		DB::unprepared(file_get_contents("src/sql_dump/lib_book_transaction_financial.sql"));
+
+
+		DB::unprepared(file_get_contents("src/sql_dump/acc_codesparam.sql"));
+		DB::unprepared(file_get_contents("src/sql_dump/acc_codesparam.sql"));
+		DB::unprepared(file_get_contents("src/sql_dump/acc_codesparam.sql"));
+		DB::unprepared(file_get_contents("src/sql_dump/acc_codesparam.sql"));
 
 	}
 
