d5bc55db1a50364d84225cfaabe9515a23ed4a83
tanin <tanin@edutechsolutionsbd.com>
2015-08-20 13:43:18 +0600

    user profile

--- app/modules/user/views/profile/profile.blade.php
+++ app/modules/user/views/profile/profile.blade.php
@@ -13,32 +13,32 @@
         </div>
         <section class="col-lg-12 connectedSortable"style="background-color:#ffffff">
             <section class="col-lg-4 connectedSortable">
-             <p>&nbsp;</p>
-              <h4><b style="color: #000000">Personal Information</b></h4>
-               <span class="text-muted ">You Can Change Your Profile Picture From Here .  <a href="{{Route('user/profile-info/profile-image',['id'=>$userProfile->id])}}"data-toggle="modal" data-target="#changeImageModal">Change Profile Picture</a></span>
+                <p>&nbsp;</p>
+                <h4><b style="color: #000000">Personal Information</b></h4>
+                <span class="text-muted ">You Can Change Your Profile Picture From Here .  <a href="{{Route('user/profile-info/profile-image',['id'=>$userProfile->id])}}"data-toggle="modal" data-target="#changeImageModal">Change Profile Picture</a></span>
             </section>
             <div class="box box-solid">
                 <section class="col-lg-8 connectedSortable pull-right">
                     <br>
                     <div class="box box-info">
                          <div class="box-body" style="padding: 30px;background-color:aliceblue">
-                                 <div class="col-lg-6 pull-left">
-                                    {{ HTML::image( "/user_images/profile/".$userProfile->image , 'User Image', ['class'=>'img-circle']) }}
-                                 </div>
-                                 <br>
-                                 <div class="col-lg-6 pull-right">
-                                     @if(isset($userProfile))
-                                         First Name : <b>{{$userProfile->first_name}} </b><br>
-                                         Last Name : <b>{{$userProfile->last_name}}</b><br>
-                                         Date of Birth : {{$userProfile->date_of_birth}}<br>
-                                         Gender : {{$userProfile->gender}}<br>
-                                         City : {{$userProfile->city}}<br>
-                                         Zip Code : {{$userProfile->zip_code}}<br>
-                                         Country : {{$userProfile->relCountry->title}}
-                                     @else
-                                         {{"No Profile data found !"}}
-                                     @endif
-                                 </div>
+                             <div class="col-lg-6 pull-left">
+                                {{ HTML::image( "/user_images/profile/".$userProfile->image , 'User Image', ['class'=>'img-circle']) }}
+                             </div>
+                             <br>
+                             <div class="col-lg-6 pull-right">
+                                 @if(isset($userProfile))
+                                     First Name : <b>{{$userProfile->first_name}} </b><br>
+                                     Last Name : <b>{{$userProfile->last_name}}</b><br>
+                                     Date of Birth : {{$userProfile->date_of_birth}}<br>
+                                     Gender : {{$userProfile->gender}}<br>
+                                     City : {{$userProfile->city}}<br>
+                                     Zip Code : {{$userProfile->zip_code}}<br>
+                                     Country : {{$userProfile->relCountry->title}}
+                                 @else
+                                     {{"No Profile data found !"}}
+                                 @endif
+                             </div>
                              <p>&nbsp;</p>
                              <div>
                                  <a class="pull-right btn btn-sm btn-default" href="{{ URL::route('user/edit/profile-info',['id'=>$userProfile->id]) }}" data-toggle="modal" data-target="#modal" >Edit <i class="fa fa-arrow-circle-right"></i></a>
@@ -47,11 +47,9 @@
                     </div>
                     <p>&nbsp;</p>
                     {{--New Entry--}}
+
                     <div class="box box-info">
                          <div class="box-body" style="padding: 30px;background-color:aliceblue">
-                                 {{--<div class="col-lg-6 pull-left">
-                                    {{ HTML::image( "/user_images/profile/".$userProfile->image , 'User Image', ['class'=>'img-circle']) }}
-                                 </div>--}}
                                  <br>
                                  <div class="col-lg-6 pull-right">
                                      @if(isset($userMeta))
@@ -65,14 +63,6 @@
                                      @endif
                                  </div>
                              <p>&nbsp;</p>
-                             <div>
-                             {{--@if($userMeta != null)
-                                 <a class="pull-right btn btn-sm btn-default" href="{{ URL::route('user/edit/profile-info',['id'=>$userMeta->id]) }}" data-toggle="modal" data-target="#modal" >Edit <i class="fa fa-arrow-circle-right"></i></a>
-                             @else
-                                 <a class="pull-right btn btn-sm btn-default" href="{{ URL::route('user/edit/profile-info',['id'=>$userMeta->id]) }}" data-toggle="modal" data-target="#modal" >Edit <i class="fa fa-arrow-circle-right"></i></a>
-                             @endif--}}
-                                 {{--<a class="pull-right btn btn-sm btn-default" href="{{ URL::route('user/edit/profile-info',['id'=>$userMeta->id]) }}" data-toggle="modal" data-target="#modal" >Edit <i class="fa fa-arrow-circle-right"></i></a>--}}
-                             </div>
                          </div>
                     </div>
                     <p>&nbsp;</p>
