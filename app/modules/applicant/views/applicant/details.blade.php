e5e5a99031b8b3ab3c413c5de1b3d10115e3eae1
tanin <tanin@edutechsolutionsbd.com>
2015-08-25 17:43:53 +0600

    applicant_details

--- app/modules/applicant/views/applicant/details.blade.php
+++ app/modules/applicant/views/applicant/details.blade.php
@@ -94,7 +94,7 @@
                             @if($applicant_personal_info != null)
                                 <a style="margin-left:-21%" class="pull-right btn btn-default" href="{{ URL::route('admission.public.applicant-profile-edit', ['id'=>$applicant_personal_info->id]  ) }}" data-toggle="modal" data-target="#addDegreeModal" >Edit</a>
                             @else
-                                <a style="margin-left:-21%" class="pull-right btn btn-default" href="{{ URL::to('applicant/personal_info/')}}" >Add Profile Information</a>
+                                <a style="margin-left:-21%" class="pull-right btn btn-default" href="{{ URL::to('applicant/profile/')}}" >Add Profile Information</a>
                             @endif
                             @if(isset($applicant_personal_info))
                                  <tr>
